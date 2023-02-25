d047fcd83f7f043de1bc3529663ccdb258d77464
Andre Cv <andrecanalesv@gmail.com>
2023-02-25 10:56:00 -0500

    update

--- index.js
+++ index.js
@@ -10,23 +10,61 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json({ limit: '1000000mb' }));
 app.use(bodyParser.urlencoded({ limit: '1000000mb', extended: true }));
 
-app.post('/verify', verifyData);
-
 io.on('connection', (socket) => {
-    socket.on('sendDataFront', (msg) => {
-        console.log(msg['id']);
+    socket.on('verifyDocument', (resData) => {
+        if ((resData || "").id == "server") {
+            var allData = JSON.parse((resData || {}).allData);
+            var receptData = JSON.parse((resData || {}).receptData);
+            var dataFront = JSON.parse(receptData.data);
+            var refTienda = receptData.local;
+            var arAllComprobantes = [];
+            var arrNotReg = [];
+            let date = new Date();
+            let day = `0${date.getDate()}`.slice(-2);
+            let month = `0${date.getMonth() + 1}`.slice(-2);
+            let year = date.getFullYear();
+
+            (allData || []).filter((dataCentral) => {
+                var newCp = (dataCentral || {}).cmpNumero.split('-');
+                (arAllComprobantes || []).push(newCp[0] + '-' + Number(newCp[1]));
+            });
+
+            (dataFront || []).filter((cmp) => {
+                var newCp = (cmp || {}).cmpSerie + '-' + (cmp || {}).cmpNumero;
+                if (!(arAllComprobantes || {}).includes(newCp)) {
+                    (arrNotReg || []).push({
+                        "CORRELATIVO": newCp,
+                        "TIPO DOCUMENTO": (cmp || {}).cmpTipo,
+                        "FECHA": (cmp || {}).cmpFecha
+                    });
+                }
+            });
+
+            console.log(`${day}-${month}-${year} - ${refTienda} - Comprobantes enviados: ${arrNotReg.length}`);
+
+            if (arrNotReg.length) {
+                const XLSX = require("xlsx");
+                const workSheet = XLSX.utils.json_to_sheet(arrNotReg);
+                const workBook = XLSX.utils.book_new();
+                XLSX.utils.book_append_sheet(workBook, workSheet, "attendance");
+                const xlsFile = XLSX.write(workBook, { bookType: "xlsx", type: "buffer" });
+                require("./send_mail")('andrecanalesv@gmail.com', `${refTienda} - FACTURAS FALTANTES EN SERVIDOR`, xlsFile)
+                    .catch(error => res.send(error));
+            }
+
+
+        }
     });
 
-    socket.on('consultingDataFront', (msg) => {
-        console.log(msg['id']);
+    socket.on('petitionFront', (data) => {
+        socket.broadcast.emit("sendDataFront", data['local']);
     });
-
-
-    console.log('a user connected');
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
+
+    console.log('a user connected');
 });
 
 server.listen(3200, () => {
