b1d7a41402ecca07fc881b13add318b1c3c7e1b0
Andre Cv <andrecanalesv@gmail.com>
2023-03-01 12:37:16 -0500

    update email

---
+++ sendEmail.js
@@ -0,0 +1,50 @@
+
+import nodemailer from 'nodemailer';
+
+class clsSendEmail {
+    sendEmail(email, nome, mensagem, tienda) {
+        let date = new Date();
+        let day = `0${date.getDate()}`.slice(-2);
+        let month = `0${date.getMonth() + 1}`.slice(-2);
+        let year = date.getFullYear();
+
+        const transport = nodemailer.createTransport({
+            service: "Gmail",
+            auth: {
+                user: 'andrecanalesv@gmail.com',
+                pass: 'mrxlnchmyxpmqqlt'
+            }
+        })
+
+        const mail = {
+            from: "IT METASPERU <andrecanalesv@gmail.com>",
+            to: email,
+            subject: `${nome}`,
+            attachments: [
+                {
+                    filename: `CP-${tienda}-${day}${month}${year}` + '.xlsx',
+                    content: Buffer.from(mensagem),
+                    contentType: 'application/octet-stream',
+                }
+            ]
+        }
+
+        return new Promise((resolve, reject) => {
+            transport.sendMail(mail)
+                .then(response => {
+                    console.log(response);
+                    transport.close();
+                    return resolve(response);
+                })
+                .catch(error => {
+                    console.log(error);
+                    transport.close();
+                    return reject(error);
+                });
+        })
+
+    }
+}
+
+const emailController = new clsSendEmail;
+export default emailController;
