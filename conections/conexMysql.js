7eadf0471f72678dda3687ef858061b35bbeda09
Andre Cv <andrecanalesv@gmail.com>
2023-03-04 10:50:38 -0500

    [update] conexion bd

--- conections/conexMysql.js
+++ conections/conexMysql.js
@@ -3,7 +3,7 @@ import { createPool } from 'mysql2/promise';
 export const pool = createPool({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'J4s0nd34d$$',
     database: 'BD_METASPERU'
 });
 
