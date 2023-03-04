96b615f5fb5d3698df2ffbe25ab7fa0cb2c38072
Andre Cv <andrecanalesv@gmail.com>
2023-03-04 11:12:33 -0500

    update

--- conections/conexMysql.js
+++ conections/conexMysql.js
@@ -2,7 +2,7 @@ import { createPool } from 'mysql2/promise';
 
 export const pool = createPool({
     host: 'localhost',
-    user: 'root',
+    user: 'dbapp',
     port: 3306,
     password: 'J4s0nd34d$$',
     database: 'BD_METASPERU'
