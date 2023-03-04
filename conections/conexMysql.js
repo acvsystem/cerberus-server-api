0849a52d4c4bc7353c8169953870165cac468ef3
Andre Cv <andrecanalesv@gmail.com>
2023-03-04 10:55:27 -0500

    update

--- conections/conexMysql.js
+++ conections/conexMysql.js
@@ -3,6 +3,7 @@ import { createPool } from 'mysql2/promise';
 export const pool = createPool({
     host: 'localhost',
     user: 'root',
+    port: 3306,
     password: 'J4s0nd34d$$',
     database: 'BD_METASPERU'
 });
