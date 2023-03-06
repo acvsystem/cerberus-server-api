6d09c221e521223f49031558978e2d4b98e93771
Andre Cv <andrecanalesv@gmail.com>
2023-03-06 11:28:16 -0500

    update

--- conections/conexMysql.js
+++ conections/conexMysql.js
@@ -5,6 +5,11 @@ export const pool = createPool({
     user: 'dbapp',
     port: 3306,
     password: 'J4s0nd34d$$',
-    database: 'BD_METASPERU'
+    database: 'BD_METASPERU',
+    waitForConnections : true , 
+    connectionLimit : 100 , 
+    maxIdle : 100 ,  
+    idleTimeout : 60000 ,
+    queueLimit : 0 
 });
 
