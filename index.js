c2065c602826dcd76dc4a34f5fe945283850bc35
acvsystem <36681777+acvsystem@users.noreply.github.com>
2023-03-21 21:14:24 -0500

    update

--- index.js
+++ index.js
@@ -65,8 +65,10 @@ io.on('connection', async (socket) => {
             socket.to(`${listClient.id}`).emit("sessionConnect", listSessionDisconnet);
         }
 
-        socket.broadcast.emit("status:serverSUNAT:send", [{'code': codeTerminal,'isConect' : '0'}]);
-        
+        if (isIcg == 'true') {
+            socket.broadcast.emit("conexion:serverICG:send", [{'code': codeTerminal,'isConect' : '0'}]);
+        }
+
         console.log(`disconnect ${codeTerminal} - idApp`, listClient.id);
         console.log('user disconnected');
     });
