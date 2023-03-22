0bfdcf205dae98f26c7375fc2bf23838d8392611
acvsystem <36681777+acvsystem@users.noreply.github.com>
2023-03-22 11:43:10 -0500

    update

--- controllers/csSessionSocket.js
+++ controllers/csSessionSocket.js
@@ -43,7 +43,7 @@ class clsSessionSocket {
 
     async disconnect(codigo) {
         await pool.query(`UPDATE TB_TERMINAL_TIENDA SET ISONLINE = false WHERE CODIGO_TERMINAL = '${codigo}'`);
-        listSession = await this.sessionOneList(codigo);
+        let listSession = await this.sessionOneList(codigo);
         return listSession;
     }
 
