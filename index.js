83cc5def2ca119277070a215457dc97e7e8c8a1e
acvsystem <36681777+acvsystem@users.noreply.github.com>
2023-03-29 12:31:18 -0500

    update

--- index.js
+++ index.js
@@ -20,87 +20,7 @@ var listClient = { id: '' };
 var agenteList = [];
 
 
-app.post('/sunat-notification', async (req, res) => {
 
-    let arrDocumento = (((req || []).body || [])[0] || {});
-
-    let [verifyDocument] = await pool.query(`SELECT * FROM TB_DOCUMENTOS_ERROR_SUNAT WHERE CODIGO_DOCUMENTO = ${(arrDocumento || {}).CODIGO_DOCUMENTO};`);
-
-    if (!(verifyDocument || []).length) {
-        await pool.query(`INSERT INTO TB_DOCUMENTOS_ERROR_SUNAT(CODIGO_DOCUMENTO,NRO_CORRELATIVO,NOM_ADQUIRIENTE,NRO_DOCUMENTO,TIPO_DOCUMENTO_ADQUIRIENTE,OBSERVACION,ESTADO_SUNAT,ESTADO_COMPROBANTE,CODIGO_ERROR_SUNAT,FECHA_EMISION)
-                        VALUES(${(arrDocumento || {}).CODIGO_DOCUMENTO},
-                        '${(arrDocumento || {}).NRO_CORRELATIVO}',
-                        '${(arrDocumento || {}).NOM_ADQUIRIENTE}',
-                        '${(arrDocumento || {}).NRO_DOCUMENTO}',
-                        '${(arrDocumento || {}).TIPO_DOCUMENTO_ADQUIRIENTE}',
-                        '${(arrDocumento || {}).OBSERVACION}',
-                        '${(arrDocumento || {}).ESTADO_SUNAT}',
-                        '${(arrDocumento || {}).ESTADO_COMPROBANTE}',
-                        '${(arrDocumento || {}).CODIGO_ERROR_SUNAT}',
-                        '${(arrDocumento || {}).FECHA_EMISION}');`);
-
-        res.send('RECEPCION EXITOSA..!!');
-    } else {
-        await pool.query(`UPDATE TB_DOCUMENTOS_ERROR_SUNAT SET
-                        NOM_ADQUIRIENTE ='${(arrDocumento || {}).NOM_ADQUIRIENTE}',
-                        NRO_DOCUMENTO = '${(arrDocumento || {}).NRO_DOCUMENTO}',
-                        TIPO_DOCUMENTO_ADQUIRIENTE = '${(arrDocumento || {}).TIPO_DOCUMENTO_ADQUIRIENTE}',
-                        OBSERVACION = '${(arrDocumento || {}).OBSERVACION}',
-                        ESTADO_SUNAT = '${(arrDocumento || {}).ESTADO_SUNAT}',
-                        ESTADO_COMPROBANTE = '${(arrDocumento || {}).ESTADO_COMPROBANTE}',
-                        CODIGO_ERROR_SUNAT = '${(arrDocumento || {}).CODIGO_ERROR_SUNAT}'`);
-        res.send('RECEPCION EXITOSA..!!');
-    }
-
-    let [documentList] = await pool.query(`SELECT * FROM TB_DOCUMENTOS_ERROR_SUNAT;`);
-    //socket.emit("sendNotificationSunat", documentList);
-
-    /*let errDocument = [
-        {
-            'ID.FACTURA': (arrDocumento || {}).CODIGO_DOCUMENTO,
-            'NUM.FACTURA': (arrDocumento || {}).NRO_CORRELATIVO,
-            'FEC.EMISION': (arrDocumento || {}).FECHA_EMISION,
-            'NOM.CLIENTE': (arrDocumento || {}).NOM_ADQUIRIENTE,
-            'NUM.DOCUMENTO': (arrDocumento || {}).NRO_DOCUMENTO,
-        }
-    ]*/
-
-    var bodyHTML = `<p>Buenos días, adjunto los datos de una factura emitida con numero de RUC errado (Cliente Con DNI, lo cual está prohibido para el caso de factura, para esos casos existen las boletas).</p> 
-
-    <p>Lamentablemente no han cumplido con los procesos y métodos de validación que se les han proporcionado.</p>  
-    
-    <p>Quedo atento de la persona responsable de emitir dicha factura.</p>  
-    
-    <p>Realizar la NC con anticipo y/o vale.  Si tienen alguna inquietud me dejan saber.</p>
-    
-    <p>Saludos.
-
-    <p><strong>Datos de factura emitida:</strong></p>
-    
-    <table align="left" cellspacing="0">
-        <thead>
-            <tr>
-                <th style="border: 1px solid #9E9E9E;border-right:0px" width="110px">ID.FACTURA</th>
-                <th style="border: 1px solid #9E9E9E;border-right:0px" width="110px">NUM.FACTURA</th>
-                <th style="border: 1px solid #9E9E9E;border-right:0px" width="110px">FEC.EMISION</th>
-                <th style="border: 1px solid #9E9E9E;border-right:0px" width="200px">NOM.CLIENTE</th>
-                <th style="border: 1px solid #9E9E9E" width="140px">NUM.DOCUMENTO</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td style="border: 1px solid #9E9E9E;border-top:0px;text-align:center;border-right:0px">${(arrDocumento || {}).CODIGO_DOCUMENTO}</td>
-                <td style="border: 1px solid #9E9E9E;border-top:0px;text-align:center">${(arrDocumento || {}).NRO_CORRELATIVO}</td>
-                <td style="border: 1px solid #9E9E9E;border-top:0px;text-align:center;border-right:0px">${(arrDocumento || {}).FECHA_EMISION}</td>
-                <td style="border: 1px solid #9E9E9E;border-top:0px;text-align:center;border-right:0px">${(arrDocumento || {}).NOM_ADQUIRIENTE}</td>
-                <td style="border: 1px solid #9E9E9E;border-top:0px;text-align:center">${(arrDocumento || {}).NRO_DOCUMENTO}</td>
-            </tr>
-        </tbody>
-    </table>`;
-
-    emailController.sendEmail('', `FACTURA CON RUC ERRADO`, bodyHTML, null, null)
-        .catch(error => res.send(error));
-});
 
 io.on('connection', async (socket) => {
     let codeQuery = socket.handshake.query.code;
@@ -177,6 +97,89 @@ io.on('connection', async (socket) => {
                 .catch(error => res.send(error));
         }
     }
+    
+
+    app.post('/sunat-notification', async (req, res) => {
+
+        let arrDocumento = (((req || []).body || [])[0] || {});
+    
+        let [verifyDocument] = await pool.query(`SELECT * FROM TB_DOCUMENTOS_ERROR_SUNAT WHERE CODIGO_DOCUMENTO = ${(arrDocumento || {}).CODIGO_DOCUMENTO};`);
+    
+        if (!(verifyDocument || []).length) {
+            await pool.query(`INSERT INTO TB_DOCUMENTOS_ERROR_SUNAT(CODIGO_DOCUMENTO,NRO_CORRELATIVO,NOM_ADQUIRIENTE,NRO_DOCUMENTO,TIPO_DOCUMENTO_ADQUIRIENTE,OBSERVACION,ESTADO_SUNAT,ESTADO_COMPROBANTE,CODIGO_ERROR_SUNAT,FECHA_EMISION)
+                            VALUES(${(arrDocumento || {}).CODIGO_DOCUMENTO},
+                            '${(arrDocumento || {}).NRO_CORRELATIVO}',
+                            '${(arrDocumento || {}).NOM_ADQUIRIENTE}',
+                            '${(arrDocumento || {}).NRO_DOCUMENTO}',
+                            '${(arrDocumento || {}).TIPO_DOCUMENTO_ADQUIRIENTE}',
+                            '${(arrDocumento || {}).OBSERVACION}',
+                            '${(arrDocumento || {}).ESTADO_SUNAT}',
+                            '${(arrDocumento || {}).ESTADO_COMPROBANTE}',
+                            '${(arrDocumento || {}).CODIGO_ERROR_SUNAT}',
+                            '${(arrDocumento || {}).FECHA_EMISION}');`);
+    
+            res.send('RECEPCION EXITOSA..!!');
+        } else {
+            await pool.query(`UPDATE TB_DOCUMENTOS_ERROR_SUNAT SET
+                            NOM_ADQUIRIENTE ='${(arrDocumento || {}).NOM_ADQUIRIENTE}',
+                            NRO_DOCUMENTO = '${(arrDocumento || {}).NRO_DOCUMENTO}',
+                            TIPO_DOCUMENTO_ADQUIRIENTE = '${(arrDocumento || {}).TIPO_DOCUMENTO_ADQUIRIENTE}',
+                            OBSERVACION = '${(arrDocumento || {}).OBSERVACION}',
+                            ESTADO_SUNAT = '${(arrDocumento || {}).ESTADO_SUNAT}',
+                            ESTADO_COMPROBANTE = '${(arrDocumento || {}).ESTADO_COMPROBANTE}',
+                            CODIGO_ERROR_SUNAT = '${(arrDocumento || {}).CODIGO_ERROR_SUNAT}'`);
+            res.send('RECEPCION EXITOSA..!!');
+        }
+    
+        let [documentList] = await pool.query(`SELECT * FROM TB_DOCUMENTOS_ERROR_SUNAT;`);
+        socket.emit("sendNotificationSunat", documentList);
+    
+        /*let errDocument = [
+            {
+                'ID.FACTURA': (arrDocumento || {}).CODIGO_DOCUMENTO,
+                'NUM.FACTURA': (arrDocumento || {}).NRO_CORRELATIVO,
+                'FEC.EMISION': (arrDocumento || {}).FECHA_EMISION,
+                'NOM.CLIENTE': (arrDocumento || {}).NOM_ADQUIRIENTE,
+                'NUM.DOCUMENTO': (arrDocumento || {}).NRO_DOCUMENTO,
+            }
+        ]*/
+    
+        var bodyHTML = `<p>Buenos días, adjunto los datos de una factura emitida con numero de RUC errado (Cliente Con DNI, lo cual está prohibido para el caso de factura, para esos casos existen las boletas).</p> 
+    
+        <p>Lamentablemente no han cumplido con los procesos y métodos de validación que se les han proporcionado.</p>  
+        
+        <p>Quedo atento de la persona responsable de emitir dicha factura.</p>  
+        
+        <p>Realizar la NC con anticipo y/o vale.  Si tienen alguna inquietud me dejan saber.</p>
+        
+        <p>Saludos.
+    
+        <p><strong>Datos de factura emitida:</strong></p>
+        
+        <table align="left" cellspacing="0">
+            <thead>
+                <tr>
+                    <th style="border: 1px solid #9E9E9E;border-right:0px" width="110px">ID.FACTURA</th>
+                    <th style="border: 1px solid #9E9E9E;border-right:0px" width="110px">NUM.FACTURA</th>
+                    <th style="border: 1px solid #9E9E9E;border-right:0px" width="110px">FEC.EMISION</th>
+                    <th style="border: 1px solid #9E9E9E;border-right:0px" width="200px">NOM.CLIENTE</th>
+                    <th style="border: 1px solid #9E9E9E" width="140px">NUM.DOCUMENTO</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style="border: 1px solid #9E9E9E;border-top:0px;text-align:center;border-right:0px">${(arrDocumento || {}).CODIGO_DOCUMENTO}</td>
+                    <td style="border: 1px solid #9E9E9E;border-top:0px;text-align:center">${(arrDocumento || {}).NRO_CORRELATIVO}</td>
+                    <td style="border: 1px solid #9E9E9E;border-top:0px;text-align:center;border-right:0px">${(arrDocumento || {}).FECHA_EMISION}</td>
+                    <td style="border: 1px solid #9E9E9E;border-top:0px;text-align:center;border-right:0px">${(arrDocumento || {}).NOM_ADQUIRIENTE}</td>
+                    <td style="border: 1px solid #9E9E9E;border-top:0px;text-align:center">${(arrDocumento || {}).NRO_DOCUMENTO}</td>
+                </tr>
+            </tbody>
+        </table>`;
+    
+        emailController.sendEmail('', `FACTURA CON RUC ERRADO`, bodyHTML, null, null)
+            .catch(error => res.send(error));
+    });
 
     console.log(`connect ${codeTerminal} - idApp`, listClient.id);
     console.log('a user connected');
