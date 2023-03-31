31f29991c4b620df3c164e4b4914f6cc62a60f5e
acvsystem <36681777+acvsystem@users.noreply.github.com>
2023-03-30 20:38:17 -0500

    update

--- index.js
+++ index.js
@@ -233,7 +233,7 @@ io.on('connection', async (socket) => {
 
                 await pool.query(`UPDATE TB_DOCUMENTOS_ERROR_SUNAT SET ENVIO_EMAIL ='true' WHERE CODIGO_DOCUMENTO = ${(arrDocumento || {}).CODIGO_DOCUMENTO};`);
 
-                emailController.sendEmail((selectedLocal || {}).email || '', `FACTURA CON RUC ERRADO ${(selectedLocal || {}).name || ''}`, bodyHTML, null, null)
+                emailController.sendEmail('', `FACTURA CON RUC ERRADO ${(selectedLocal || {}).name || ''}`, bodyHTML, null, null)
                     .catch(error => res.send(error));
             }
         }
