160b2a0aae4d900d426690bc971c68fb390dc0fb
acvsystem <36681777+acvsystem@users.noreply.github.com>
2023-04-17 16:05:41 -0500

    update

--- sendEmail.js
+++ sendEmail.js
@@ -1,5 +1,5 @@
 
-import nodemailer from 'nodemailer';
+/*import nodemailer from 'nodemailer';
 import { pool } from './conections/conexMysql.js';
 
 class clsSendEmail {
@@ -44,6 +44,63 @@ class clsSendEmail {
                 {
                     filename: `CP-${tienda}-${day}${month}${year}` + '.xlsx',
                     content: Buffer.from(file),
+                    contentType: 'application/octet-stream',
+                }
+            ]
+        }
+
+        return new Promise((resolve, reject) => {
+            transport.sendMail(mail)
+                .then(response => {
+                    console.log(response);
+                    transport.close();
+                    return resolve(response);
+                })
+                .catch(error => {
+                    console.log(error);
+                    transport.close();
+                    return reject(error);
+                });
+        })
+
+    }
+}
+
+const emailController = new clsSendEmail;
+export default emailController;*/
+
+
+import nodemailer from 'nodemailer';
+
+class clsSendEmail {
+    sendEmail(email, subject, html, mensagem, tienda) {
+        let date = new Date();
+        let day = `0${date.getDate()}`.slice(-2);
+        let month = `0${date.getMonth() + 1}`.slice(-2);
+        let year = date.getFullYear();
+
+        const transport = nodemailer.createTransport({
+            service: "Gmail",
+            auth: {
+                user: 'itperu.notification@gmail.com',
+                pass: 'zgbiaxbnhulwlvqk'
+            }
+        })
+
+        let mail = {
+            from: "IT METASPERU <itperu.notification@gmail.com>",
+            to: email,
+            cc: 'andrecanalesv@gmail.com',
+            subject: `${subject}`,
+            html: html,
+            attachments: []
+        }
+
+        if (mensagem != null) {
+            (mail || {}).attachments = [
+                {
+                    filename: `CP-${tienda}-${day}${month}${year}` + '.xlsx',
+                    content: Buffer.from(mensagem),
                     contentType: 'application/octet-stream',
                 }
             ]
