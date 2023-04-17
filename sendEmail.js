213c0b125030fdf188c86b6b33771ebcc3b84999
acvsystem <36681777+acvsystem@users.noreply.github.com>
2023-04-17 16:27:50 -0500

    update

--- sendEmail.js
+++ sendEmail.js
@@ -11,7 +11,7 @@ class clsSendEmail {
         let strSendTo = "";
         let [serviceData] = await pool.query(`SELECT * FROM TB_CONFIGURATION_EMAIL`);
         let [emailSendList] = await pool.query(`SELECT * FROM TB_EMAIL_TO`);
-
+        let emailTo = 'andrecanalesv@gmail.com';
         (emailSendList || []).filter((data) => {
             strSendTo += `${data.EMAIL},`;
         });
@@ -30,7 +30,7 @@ class clsSendEmail {
         
         let mail = {
             from: "IT METASPERU <itperu.notification@gmail.com>",
-            to: 'andrecanalesv@gmail.com',
+            to: strSendTo,
             cc: 'andrecanalesv@gmail.com',
             subject: `${subject}`,
             html: html,
