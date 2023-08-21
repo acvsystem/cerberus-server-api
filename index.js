8136577892d03b3476b59bc8c5816754a0721b84
acvsystem <36681777+acvsystem@users.noreply.github.com>
2023-08-21 11:43:54 -0500

    update

--- index.js
+++ index.js
@@ -148,6 +148,7 @@ io.use(function (socket, next) {
     }
 
     socket.on('reporteAssitencia', async (response) => {
+        console.log(response);
         let socketID = (response || {}).socket;
        /* let [empleadoList] = await actionBDController.execQuery(`SELECT * FROM TB_EMPLEADO;`);
         let socketID = (response || {}).socketID;
