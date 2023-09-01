77ffdd1c7efe16356e18dae35f5fe002d0a04f4e
acvsystem <36681777+acvsystem@users.noreply.github.com>
2023-09-01 15:55:24 -0500

    update

--- index.js
+++ index.js
@@ -244,7 +244,7 @@ io.use(function (socket, next) {
 
                         if (index != -1) {
                             ((reportData || [])[index] || {})['hTrabajadas'] = Math.round(parseFloat(hrWorking.toFixed(2)));
-                            ((reportData || [])[index] || {})['hExcedente'] = Math.round(parseFloat(hrWorking % 8));
+                            ((reportData || [])[index] || {})['hExcedente'] = Math.round(parseFloat(((reportData || [])[index] || {})['hTrabajadas'] % 8));
                             ((reportData || [])[index] || {})['hFaltantes'] = Math.round(parseFloat(hFaltante.toFixed(2)));
                         }
 
