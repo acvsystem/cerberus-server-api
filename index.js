ee2085e51bc9f7ee08f353c7d3ab6f4f6d94581a
acvsystem <36681777+acvsystem@users.noreply.github.com>
2023-09-01 15:53:08 -0500

    update

--- index.js
+++ index.js
@@ -191,7 +191,7 @@ io.use(function (socket, next) {
 
                     nroTransacciones += asits.nroVentas;
 
-                    if (hrWorking > 8) {
+                    if (Math.round(parseFloat(hrWorking.toFixed(2))) > 8) {
                         hExcedente += hrWorking % 8;
                     }
 
@@ -244,7 +244,7 @@ io.use(function (socket, next) {
 
                         if (index != -1) {
                             ((reportData || [])[index] || {})['hTrabajadas'] = Math.round(parseFloat(hrWorking.toFixed(2)));
-                            ((reportData || [])[index] || {})['hExcedente'] = Math.round(parseFloat(hrWorking % 8));
+                            ((reportData || [])[index] || {})['hExcedente'] = Math.round(parseFloat(hExcedente.toFixed(2)));
                             ((reportData || [])[index] || {})['hFaltantes'] = Math.round(parseFloat(hFaltante.toFixed(2)));
                         }
 
