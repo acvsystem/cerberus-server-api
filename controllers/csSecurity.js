21757e78829ee729e8499b92093a7feb966aaa8f
acvsystem <36681777+acvsystem@users.noreply.github.com>
2023-09-21 15:27:04 -0500

    update

--- controllers/csSecurity.js
+++ controllers/csSecurity.js
@@ -8,7 +8,7 @@ export const Login = async (req, res) => {
     let objLogin = req.body;
     let usuario = objLogin['usuario'].replace(/[^a-zA-Z-0-9 ]/g, "");
     let password = objLogin['password'];
-    const [dataUser] = await pool.query(`SELECT NOMBRE, NM_NIVEL, ID_NVL_ACCESS FROM TB_PROFILE_USER 
+    const [dataUser] = await pool.query(`SELECT ID_LOGIN, NOMBRE, NM_NIVEL, ID_NVL_ACCESS FROM TB_PROFILE_USER 
                                         INNER JOIN TB_LOGIN ON TB_LOGIN.ID_LOGIN = TB_PROFILE_USER.FK_ID_LOGIN 
                                         INNER JOIN TB_NIVEL_ACCESS ON TB_NIVEL_ACCESS.ID_NVL_ACCESS = TB_LOGIN.FK_ID_NVL_ACCESS
                                         WHERE DESC_USUARIO = '${usuario}' AND PASSWORD = '${password}'`)
@@ -19,7 +19,7 @@ export const Login = async (req, res) => {
         const [menuUser] = await pool.query(`SELECT NAME_MENU,RUTE_PAGE FROM TB_PROFILE_USER 
             INNER JOIN TB_LOGIN ON TB_LOGIN.ID_LOGIN = TB_PROFILE_USER.FK_ID_LOGIN 
             INNER JOIN TB_NIVEL_ACCESS ON TB_NIVEL_ACCESS.ID_NVL_ACCESS = TB_LOGIN.FK_ID_NVL_ACCESS
-            INNER JOIN TB_MENU_SISTEMA ON TB_MENU_SISTEMA.FK_ID_LOGIN_MENU = TB_LOGIN.ID_LOGIN WHERE TB_NIVEL_ACCESS.ID_NVL_ACCESS = ${((dataUser || [])[0] || {}).ID_NVL_ACCESS};`);
+            INNER JOIN TB_MENU_SISTEMA ON TB_MENU_SISTEMA.FK_ID_LOGIN_MENU = TB_LOGIN.ID_LOGIN WHERE TB_NIVEL_ACCESS.ID_NVL_ACCESS = ${((dataUser || [])[0] || {}).ID_NVL_ACCESS} AND TB_LOGIN.ID_LOGIN = ${((dataUser || [])[0] || {}).NM_NIVEL};`);
 
         const token = tokenController.createToken(usuario, nivelUser);
 
