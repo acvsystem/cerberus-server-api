1c6d8c50728d98da481cf0fbe8144a4600781ff4
acvsystem <36681777+acvsystem@users.noreply.github.com>
2023-11-21 15:51:45 -0500

    update

--- index.js
+++ index.js
@@ -179,22 +179,21 @@ io.use(function (socket, next) {
     let socketID = (configurationList || {}).socket;
     let dataEmpServidor = JSON.parse((response || {}).serverData);
     let dataEmployee = [];
-    const boolArray = await Promise.all(
-     await (dataEmpServidor || []).filter(async (empSrv, i) => {
-        if (
-          (empSrv || {}).nroDocumento != "" &&
-          (empSrv || {}).nroDocumento != null
-        ) {
-          let existEMP = await actionBDController.verificationRegister(
-            "TB_EMPLEADO",
-            `NRO_DOC = '${(empSrv || {}).nroDocumento}';`
-          );
+    (dataEmpServidor || []).filter(async (empSrv, i) => {
+      if (
+        (empSrv || {}).nroDocumento != "" &&
+        (empSrv || {}).nroDocumento != null
+      ) {
+        let existEMP = await actionBDController.verificationRegister(
+          "TB_EMPLEADO",
+          `NRO_DOC = '${(empSrv || {}).nroDocumento}';`
+        );
 
-          if (!existEMP.length) {
-            console.log(empSrv);
+        if (!existEMP.length) {
+          console.log(empSrv);
 
-            dataEmployee.push(empSrv);
-            /*  await actionBDController.execQuery(`INSERT INTO TB_EMPLEADO(
+          dataEmployee.push(empSrv);
+          /*  await actionBDController.execQuery(`INSERT INTO TB_EMPLEADO(
                                           CODIGO_ICG,
                                           CODIGO_EJB,
                                           AP_PATERNO,
@@ -228,17 +227,13 @@ io.use(function (socket, next) {
                                               0.0,
                                               ""
                                           );`);*/
-          }
         }
-        console.log(dataEmpServidor.length -1,i);
-        if(dataEmpServidor.length -1 == i){
-            console.log(dataEmployee);
-            return
-        }
-      })
-    );
+      }
 
-    socket.to(`${socketID}`).emit("sendUDPEmpleados", dataEmployee);
+      if (dataEmpServidor.length - 1 == i) {
+        socket.to(`${socketID}`).emit("sendUDPEmpleados", dataEmployee);
+      }
+    });
   });
 
   socket.on("reporteAssitencia", async (response) => {
