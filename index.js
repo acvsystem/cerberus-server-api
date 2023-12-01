6566d12bd381f9967a9238e34bb4dc3498f496ba
acvsystem <36681777+acvsystem@users.noreply.github.com>
2023-12-01 17:12:40 -0500

    update

--- index.js
+++ index.js
@@ -332,7 +332,7 @@ io.use(function (socket, next) {
     let orginDataNoFound = [];
 
     dataAsistensList.filter((nm)=>{
-      if(nm.nombreCompleto == 'BAYLON RIOS ALEJANDRA BRUNELA'){
+      if(nm.nombreCompleto == 'ALEJANDRA BRUNELA BAYLON RIOS'){
         console.log(nm);
       }
       if(nm.nombreCompleto != 'Compras Peru' && nm.nombreCompleto != 'GLOBAL' && nm.nombreCompleto != 'IT'){
