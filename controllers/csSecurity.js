c2bd97f30cca23c1108b5b0ee6895a4e9a52532d
acvsystem <36681777+acvsystem@users.noreply.github.com>
2023-12-23 09:18:03 -0500

    update

--- controllers/csSecurity.js
+++ controllers/csSecurity.js
@@ -106,14 +106,14 @@ export const createAccessPostulant = async (req, res) => {
 
   if ((tokenDecode || {}).isValid) {
     let privateKey = prop.keyCrypt;
-    console.log("verificationToken", `${privateKey}`);
+  
     let option = {
       expiresIn: "10800s",
       issuer: "cerberus.server",
       audience: `${((tokenDecode || {}).decoded || {}).aud}`,
     };
-    console.log("createAccessPostulant", prop);
-    const token = Jwt.sign(      { id: (option || {}).audience },
+
+    const token = Jwt.sign({ id: (option || {}).audience },
       `${privateKey}`,
       option
     );
@@ -132,7 +132,7 @@ export const createAccessPostulant = async (req, res) => {
 
     request(options, function (error, response) {
       if (error) throw new Error(error);
-      console.log((((response || {}).body || {}))["shorturl"]);
+      console.log("createAccessPostulant",(((response || {}).body || {})));
       urlAccess = JSON.parse(((response || {}).body || {}))["shorturl"] || `http://159.65.226.239:5000/postulante/${token}`;
       res.json(urlAccess);
     });
