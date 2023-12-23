d96f2f7822c28706ea3db7347a6cfeb4e47f8def
acvsystem <36681777+acvsystem@users.noreply.github.com>
2023-12-23 09:14:46 -0500

    update

--- controllers/csSecurity.js
+++ controllers/csSecurity.js
@@ -132,8 +132,7 @@ export const createAccessPostulant = async (req, res) => {
 
     request(options, function (error, response) {
       if (error) throw new Error(error);
-      console.log(error);
-      console.log(response);
+      console.log((((response || {}).body || {}))["shorturl"]);
       urlAccess = JSON.parse(((response || {}).body || {}))["shorturl"] || `http://159.65.226.239:5000/postulante/${token}`;
       res.json(urlAccess);
     });
