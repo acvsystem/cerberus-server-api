0c0baacd9e7ed43b425c1c59ac467983edb170c5
acvsystem <36681777+acvsystem@users.noreply.github.com>
2024-01-25 11:44:55 -0500

    update

--- index.js
+++ index.js
@@ -23,18 +23,18 @@ var listClient = { id: '' };
 var agenteList = [];
 
 const task_1 = cron.schedule('00 10 * * *', () => {
-    console.log('00 10');
-    emitVerificationDoc();
+  console.log('00 10');
+  emitVerificationDoc();
 });
 
 const task_2 = cron.schedule('00 15 * * *', () => {
-    console.log('00 15');
-    emitVerificationDoc();
+  console.log('00 15');
+  emitVerificationDoc();
 });
 
 const task_3 = cron.schedule('00 19 * * *', () => {
-    console.log('00 19');
-    emitVerificationDoc();
+  console.log('00 19');
+  emitVerificationDoc();
 });
 
 task_1.start();
@@ -42,134 +42,134 @@ task_2.start();
 task_3.start();
 
 function emitVerificationDoc() {
-    io.emit('consultingToFront', 'emitVerificationDoc');
+  io.emit('consultingToFront', 'emitVerificationDoc');
 }
 
 io.on('connection', async (socket) => {
-    let codeQuery = socket.handshake.query.code;
-    let codeTerminal = socket.handshake.headers.code;
-    let isIcg = socket.handshake.headers.icg;
+  let codeQuery = socket.handshake.query.code;
+  let codeTerminal = socket.handshake.headers.code;
+  let isIcg = socket.handshake.headers.icg;
 
-    let indexAgente = (agenteList || []).findIndex((data, i) => (data || {}).code == codeTerminal);
+  let indexAgente = (agenteList || []).findIndex((data, i) => (data || {}).code == codeTerminal);
 
-    if (indexAgente != -1) {
-        agenteList[indexAgente]['id'] = socket.id;
-    } else {
-        agenteList.push({ id: socket.id, code: codeTerminal });
+  if (indexAgente != -1) {
+    agenteList[indexAgente]['id'] = socket.id;
+  } else {
+    agenteList.push({ id: socket.id, code: codeTerminal });
+  }
+
+  if (codeQuery == 'app') {
+    console.log('app', socket.id);
+    listClient.id = socket.id;
+    let listSessionConnect = await sessionSocket.connect();
+    socket.emit("sessionConnect", listSessionConnect);
+    let [documentList] = await pool.query(`SELECT * FROM TB_DOCUMENTOS_ERROR_SUNAT;`);
+    socket.emit("sendNotificationSunat", documentList);
+  }
+
+  socket.on('verifyDocument', async (resData) => {
+    if ((resData || "").id == "server") {
+      let listSessionConnect = await facturacionController.verificacionDocumentos(resData);
+      socket.to(`${listClient.id}`).emit("sessionConnect", listSessionConnect);
+    }
+  });
+
+  socket.on('petitionFront', (data) => {
+    let selectAgente = (agenteList || []).find((data) => (data || {}).id == socket.id);
+    if (typeof codeTerminal != 'undefined' && codeTerminal != '') {
+      socket.broadcast.emit("sendDataFront", data, codeTerminal);
+    }
+  });
+
+  socket.on('resTransaction', (data) => {
+    console.log(data);
+    let selectAgente = (agenteList || []).find((data) => (data || {}).id == socket.id);
+    socket.broadcast.emit("dataTransaction", data, codeTerminal);
+
+  });
+
+  socket.on('emitTransaction', (data) => {
+    console.log('emitTransaction');
+    socket.broadcast.emit("searchTransaction", 'ready');
+  });
+
+
+  socket.on('comunicationFront', (data) => {
+    console.log('comunicationFront');
+    socket.broadcast.emit("consultingToFront", 'ready');
+  });
+
+  socket.on('conexion:serverICG', (data) => {
+    socket.broadcast.emit("conexion:serverICG:send", data);
+  });
+
+  socket.on('disconnect', async () => {
+    if (codeTerminal == "SRVFACT") {
+      sessionSocket.disconnectServer();
+      socket.broadcast.emit("status:serverSUNAT:send", { 'code': 'SRVFACT', 'online': 'false' });
+    } else if (isIcg != 'true') {
+      console.log(`disconnect ${codeTerminal} - idApp`, listClient.id);
+      let listSessionDisconnet = await sessionSocket.disconnect(codeTerminal);
+      socket.to(`${listClient.id}`).emit("sessionConnect", listSessionDisconnet);
     }
 
-    if (codeQuery == 'app') {
-        console.log('app', socket.id);
-        listClient.id = socket.id;
-        let listSessionConnect = await sessionSocket.connect();
-        socket.emit("sessionConnect", listSessionConnect);
-        let [documentList] = await pool.query(`SELECT * FROM TB_DOCUMENTOS_ERROR_SUNAT;`);
-        socket.emit("sendNotificationSunat", documentList);
+    if (isIcg == 'true') {
+      socket.broadcast.emit("conexion:serverICG:send", [{ 'code': codeTerminal, 'isConect': '0' }]);
     }
 
-    socket.on('verifyDocument', async (resData) => {
-        if ((resData || "").id == "server") {
-            let listSessionConnect = await facturacionController.verificacionDocumentos(resData);
-            socket.to(`${listClient.id}`).emit("sessionConnect", listSessionConnect);
-        }
-    });
 
-    socket.on('petitionFront', (data) => {
-        let selectAgente = (agenteList || []).find((data) => (data || {}).id == socket.id);
-        if (typeof codeTerminal != 'undefined' && codeTerminal != '') {
-            socket.broadcast.emit("sendDataFront", data, codeTerminal);
-        }
-    });
-  
-    socket.on('resTransaction', (data) => {
-        console.log(data);
-        let selectAgente = (agenteList || []).find((data) => (data || {}).id == socket.id);
-          socket.broadcast.emit("dataTransaction", data, codeTerminal);
-      
-    });
+    console.log('user disconnected');
+  });
 
-    socket.on('emitTransaction', (data) => {
-            socket.broadcast.emit("searchTransaction", 'ready');
-        
-    });
+  socket.on('status:serverSUNAT', (data) => {
+    socket.broadcast.emit("status:serverSUNAT:send", data);
+  });
 
 
-    socket.on('comunicationFront', (data) => {
-        console.log('comunicationFront');
-        socket.broadcast.emit("consultingToFront", 'ready');
-    });
-
-    socket.on('conexion:serverICG', (data) => {
-        socket.broadcast.emit("conexion:serverICG:send", data);
-    });
-
-    socket.on('disconnect', async () => {
-        if (codeTerminal == "SRVFACT") {
-            sessionSocket.disconnectServer();
-            socket.broadcast.emit("status:serverSUNAT:send", { 'code': 'SRVFACT', 'online': 'false' });
-        } else if (isIcg != 'true') {
-            console.log(`disconnect ${codeTerminal} - idApp`, listClient.id);
-            let listSessionDisconnet = await sessionSocket.disconnect(codeTerminal);
-            socket.to(`${listClient.id}`).emit("sessionConnect", listSessionDisconnet);
-        }
-
-        if (isIcg == 'true') {
-            socket.broadcast.emit("conexion:serverICG:send", [{ 'code': codeTerminal, 'isConect': '0' }]);
-        }
-
-
-        console.log('user disconnected');
-    });
-
-    socket.on('status:serverSUNAT', (data) => {
-        socket.broadcast.emit("status:serverSUNAT:send", data);
-    });
-
-
-    if (codeTerminal != "SRVFACT" && isIcg != 'true') {
-        let listSessionConnect = await sessionSocket.connect(codeTerminal);
-        socket.to(`${listClient.id}`).emit("sessionConnect", listSessionConnect);
-    } else {
-        if (codeTerminal == "SRVFACT") {
-            console.log('SERVIDOR', codeTerminal);
-            emailController.sendEmail('johnnygermano@grupodavid.com', `SERVIDOR FACTURACION CONECTADO..!!!!!`, null, null, `SERVIDOR FACTURACION`)
-                .catch(error => res.send(error));
-        }
+  if (codeTerminal != "SRVFACT" && isIcg != 'true') {
+    let listSessionConnect = await sessionSocket.connect(codeTerminal);
+    socket.to(`${listClient.id}`).emit("sessionConnect", listSessionConnect);
+  } else {
+    if (codeTerminal == "SRVFACT") {
+      console.log('SERVIDOR', codeTerminal);
+      emailController.sendEmail('johnnygermano@grupodavid.com', `SERVIDOR FACTURACION CONECTADO..!!!!!`, null, null, `SERVIDOR FACTURACION`)
+        .catch(error => res.send(error));
     }
+  }
 
-    app.post('/sunat-notification', async (req, res) => {
+  app.post('/sunat-notification', async (req, res) => {
 
-        let arrDocumento = (((req || []).body || [])[0] || {});
-        let tiendasList = [
-            { code: '7A', name: 'BBW JOCKEY', email: 'bbwjockeyplaza@grupodavid.com' },
-            { code: '9N', name: 'VS MALL AVENTURA', email: 'vsmallaventura@grupodavid.com' },
-            { code: '7J', name: 'BBW MALL AVENTURA', email: 'bbwmallaventura@grupodavid.com' },
-            { code: 'PC', name: 'AEO JOCKEY', email: 'americaneaglejp@grupodavid.com' },
-            { code: '7E', name: 'BBW LA RAMBLA', email: 'bbwlarambla@grupodavid.com' },
-            { code: '9D', name: 'VS LA RAMBLA', email: 'vslarambla@grupodavid.com' },
-            { code: '9B', name: 'VS PLAZA NORTE', email: 'vsplazanorte@grupodavid.com' },
-            { code: '7C', name: 'BBW SAN MIGUEL', email: 'bbwsanmiguel@grupodavid.com' },
-            { code: '9C', name: 'VS SAN MIGUEL', email: 'vssanmiguel@grupodavid.com' },
-            { code: '7D', name: 'BBW SALAVERRY', email: 'bbwsalaverry@grupodavid.com' },
-            { code: '9I', name: 'VS SALAVERRY', email: 'vssalaverry@grupodavid.com' },
-            { code: '9G', name: 'VS MALL DEL SUR', email: 'vsmalldelsur@grupodavid.com' },
-            { code: '9H', name: 'VS PURUCHUCO', email: 'vspuruchuco@grupodavid.com' },
-            { code: '9M', name: 'VS ECOMMERCE', email: 'vsecommpe@grupodavid.com' },
-            { code: '7F', name: 'BBW ECOMMERCE', email: 'bbwecommperu@grupodavid.com' },
-            { code: 'PA', name: 'AEO ECOMMERCE', email: 'aeecompe@grupodavid.com' },
-            { code: '9K', name: 'VS MEGA PLAZA', email: 'vsmegaplaza@grupodavid.com' },
-            { code: '9L', name: 'VS MINKA', email: 'vsoutletminka@grupodavid.com' },
-            { code: '9F', name: 'VSFA JOCKEY FULL', email: 'vsfajockeyplaza@grupodavid.com' },
-            { code: '7A7', name: 'BBW ASIA', email: 'bbwasia@grupodavid.com' }
-        ];
+    let arrDocumento = (((req || []).body || [])[0] || {});
+    let tiendasList = [
+      { code: '7A', name: 'BBW JOCKEY', email: 'bbwjockeyplaza@grupodavid.com' },
+      { code: '9N', name: 'VS MALL AVENTURA', email: 'vsmallaventura@grupodavid.com' },
+      { code: '7J', name: 'BBW MALL AVENTURA', email: 'bbwmallaventura@grupodavid.com' },
+      { code: 'PC', name: 'AEO JOCKEY', email: 'americaneaglejp@grupodavid.com' },
+      { code: '7E', name: 'BBW LA RAMBLA', email: 'bbwlarambla@grupodavid.com' },
+      { code: '9D', name: 'VS LA RAMBLA', email: 'vslarambla@grupodavid.com' },
+      { code: '9B', name: 'VS PLAZA NORTE', email: 'vsplazanorte@grupodavid.com' },
+      { code: '7C', name: 'BBW SAN MIGUEL', email: 'bbwsanmiguel@grupodavid.com' },
+      { code: '9C', name: 'VS SAN MIGUEL', email: 'vssanmiguel@grupodavid.com' },
+      { code: '7D', name: 'BBW SALAVERRY', email: 'bbwsalaverry@grupodavid.com' },
+      { code: '9I', name: 'VS SALAVERRY', email: 'vssalaverry@grupodavid.com' },
+      { code: '9G', name: 'VS MALL DEL SUR', email: 'vsmalldelsur@grupodavid.com' },
+      { code: '9H', name: 'VS PURUCHUCO', email: 'vspuruchuco@grupodavid.com' },
+      { code: '9M', name: 'VS ECOMMERCE', email: 'vsecommpe@grupodavid.com' },
+      { code: '7F', name: 'BBW ECOMMERCE', email: 'bbwecommperu@grupodavid.com' },
+      { code: 'PA', name: 'AEO ECOMMERCE', email: 'aeecompe@grupodavid.com' },
+      { code: '9K', name: 'VS MEGA PLAZA', email: 'vsmegaplaza@grupodavid.com' },
+      { code: '9L', name: 'VS MINKA', email: 'vsoutletminka@grupodavid.com' },
+      { code: '9F', name: 'VSFA JOCKEY FULL', email: 'vsfajockeyplaza@grupodavid.com' },
+      { code: '7A7', name: 'BBW ASIA', email: 'bbwasia@grupodavid.com' }
+    ];
 
-        if ((arrDocumento || {}).CODIGO_ERROR_SUNAT == 2800 || (arrDocumento || {}).CODIGO_ERROR_SUNAT == 1032 || (arrDocumento || {}).CODIGO_ERROR_SUNAT == 2022 || (arrDocumento || {}).CODIGO_ERROR_SUNAT == 1083) {
-            let [verifyDocument] = await pool.query(`SELECT * FROM TB_DOCUMENTOS_ERROR_SUNAT WHERE CODIGO_DOCUMENTO = ${(arrDocumento || {}).CODIGO_DOCUMENTO};`);
-            let isEmailEnvio = ((verifyDocument || [])[0] || {}).ENVIO_EMAIL || 'false';
-            console.log('verifyDocument', verifyDocument);
+    if ((arrDocumento || {}).CODIGO_ERROR_SUNAT == 2800 || (arrDocumento || {}).CODIGO_ERROR_SUNAT == 1032 || (arrDocumento || {}).CODIGO_ERROR_SUNAT == 2022 || (arrDocumento || {}).CODIGO_ERROR_SUNAT == 1083) {
+      let [verifyDocument] = await pool.query(`SELECT * FROM TB_DOCUMENTOS_ERROR_SUNAT WHERE CODIGO_DOCUMENTO = ${(arrDocumento || {}).CODIGO_DOCUMENTO};`);
+      let isEmailEnvio = ((verifyDocument || [])[0] || {}).ENVIO_EMAIL || 'false';
+      console.log('verifyDocument', verifyDocument);
 
-            if (!(verifyDocument || []).length) {
-                await pool.query(`INSERT INTO TB_DOCUMENTOS_ERROR_SUNAT(CODIGO_DOCUMENTO,NRO_CORRELATIVO,NOM_ADQUIRIENTE,NRO_DOCUMENTO,TIPO_DOCUMENTO_ADQUIRIENTE,OBSERVACION,ESTADO_SUNAT,ESTADO_COMPROBANTE,CODIGO_ERROR_SUNAT,ENVIO_EMAIL,FECHA_EMISION)
+      if (!(verifyDocument || []).length) {
+        await pool.query(`INSERT INTO TB_DOCUMENTOS_ERROR_SUNAT(CODIGO_DOCUMENTO,NRO_CORRELATIVO,NOM_ADQUIRIENTE,NRO_DOCUMENTO,TIPO_DOCUMENTO_ADQUIRIENTE,OBSERVACION,ESTADO_SUNAT,ESTADO_COMPROBANTE,CODIGO_ERROR_SUNAT,ENVIO_EMAIL,FECHA_EMISION)
                                 VALUES(${(arrDocumento || {}).CODIGO_DOCUMENTO},
                                 '${(arrDocumento || {}).NRO_CORRELATIVO}',
                                 '${(arrDocumento || {}).NOM_ADQUIRIENTE}',
@@ -182,9 +182,9 @@ io.on('connection', async (socket) => {
                                 'false',
                                 '${(arrDocumento || {}).FECHA_EMISION}');`);
 
-                res.send('RECEPCION EXITOSA..!!');
-            } else {
-                await pool.query(`UPDATE TB_DOCUMENTOS_ERROR_SUNAT SET
+        res.send('RECEPCION EXITOSA..!!');
+      } else {
+        await pool.query(`UPDATE TB_DOCUMENTOS_ERROR_SUNAT SET
                                 NOM_ADQUIRIENTE ='${(arrDocumento || {}).NOM_ADQUIRIENTE}',
                                 NRO_DOCUMENTO = '${(arrDocumento || {}).NRO_DOCUMENTO}',
                                 TIPO_DOCUMENTO_ADQUIRIENTE = '${(arrDocumento || {}).TIPO_DOCUMENTO_ADQUIRIENTE}',
@@ -192,23 +192,23 @@ io.on('connection', async (socket) => {
                                 ESTADO_SUNAT = '${(arrDocumento || {}).ESTADO_SUNAT}',
                                 ESTADO_COMPROBANTE = '${(arrDocumento || {}).ESTADO_COMPROBANTE}',
                                 CODIGO_ERROR_SUNAT = '${(arrDocumento || {}).CODIGO_ERROR_SUNAT}' WHERE CODIGO_DOCUMENTO = ${(arrDocumento || {}).CODIGO_DOCUMENTO};`);
-                res.send('RECEPCION EXITOSA..!!');
-            }
+        res.send('RECEPCION EXITOSA..!!');
+      }
 
-            let [documentList] = await pool.query(`SELECT * FROM TB_DOCUMENTOS_ERROR_SUNAT;`);
-            socket.to(`${listClient.id}`).emit("sendNotificationSunat", documentList);
+      let [documentList] = await pool.query(`SELECT * FROM TB_DOCUMENTOS_ERROR_SUNAT;`);
+      socket.to(`${listClient.id}`).emit("sendNotificationSunat", documentList);
 
-            /*let errDocument = [
-                {
-                    'ID.FACTURA': (arrDocumento || {}).CODIGO_DOCUMENTO,
-                    'NUM.FACTURA': (arrDocumento || {}).NRO_CORRELATIVO,
-                    'FEC.EMISION': (arrDocumento || {}).FECHA_EMISION,
-                    'NOM.CLIENTE': (arrDocumento || {}).NOM_ADQUIRIENTE,
-                    'NUM.DOCUMENTO': (arrDocumento || {}).NRO_DOCUMENTO,
-                }
-            ]*/
+      /*let errDocument = [
+          {
+              'ID.FACTURA': (arrDocumento || {}).CODIGO_DOCUMENTO,
+              'NUM.FACTURA': (arrDocumento || {}).NRO_CORRELATIVO,
+              'FEC.EMISION': (arrDocumento || {}).FECHA_EMISION,
+              'NOM.CLIENTE': (arrDocumento || {}).NOM_ADQUIRIENTE,
+              'NUM.DOCUMENTO': (arrDocumento || {}).NRO_DOCUMENTO,
+          }
+      ]*/
 
-            var bodyHTML = `<p>Buenos días, adjunto los datos de una factura emitida con numero de RUC errado.</p> 
+      var bodyHTML = `<p>Buenos días, adjunto los datos de una factura emitida con numero de RUC errado.</p> 
         
             <p>Lamentablemente no han cumplido con los procesos y métodos de validación que se les han proporcionado.</p>  
             
@@ -243,46 +243,46 @@ io.on('connection', async (socket) => {
 
 
 
-            let serie = ((arrDocumento || {}).NRO_CORRELATIVO || "").split('-')[0];
+      let serie = ((arrDocumento || {}).NRO_CORRELATIVO || "").split('-')[0];
 
-            var codigo = serie.substr(1, 3);
-            var selectedLocal = {};
-            var count = 0;
+      var codigo = serie.substr(1, 3);
+      var selectedLocal = {};
+      var count = 0;
 
 
-            while (count <= 1) {
+      while (count <= 1) {
 
-                if (count == 1) {
-                    codigo = serie.substr(1, 2);
-                }
-
-                selectedLocal = tiendasList.find((data) => data.code == codigo) || {};
-                if (Object.keys(selectedLocal).length) {
-                    count = 1;
-                }
-
-                count++;
-            }
-
-            if (Object.keys(selectedLocal).length && isEmailEnvio != 'true') {
-                console.log("sunat:codigo_tienda", codigo);
-                console.log("sunat:tienda", selectedLocal);
-
-                await pool.query(`UPDATE TB_DOCUMENTOS_ERROR_SUNAT SET ENVIO_EMAIL ='true' WHERE CODIGO_DOCUMENTO = ${(arrDocumento || {}).CODIGO_DOCUMENTO};`);
-
-                emailController.sendEmail([(selectedLocal || {}).email || '', 'johnnygermano@grupodavid.com', 'josecarreno@grupodavid.com'], `FACTURA CON RUC ERRADO ${(selectedLocal || {}).name || ''}`, bodyHTML, null, null)
-                    .catch(error => res.send(error));
-            }
+        if (count == 1) {
+          codigo = serie.substr(1, 2);
         }
 
-    });
+        selectedLocal = tiendasList.find((data) => data.code == codigo) || {};
+        if (Object.keys(selectedLocal).length) {
+          count = 1;
+        }
 
-    console.log(`connect ${codeTerminal} - idApp`, listClient.id);
-    console.log('a user connected');
+        count++;
+      }
+
+      if (Object.keys(selectedLocal).length && isEmailEnvio != 'true') {
+        console.log("sunat:codigo_tienda", codigo);
+        console.log("sunat:tienda", selectedLocal);
+
+        await pool.query(`UPDATE TB_DOCUMENTOS_ERROR_SUNAT SET ENVIO_EMAIL ='true' WHERE CODIGO_DOCUMENTO = ${(arrDocumento || {}).CODIGO_DOCUMENTO};`);
+
+        emailController.sendEmail([(selectedLocal || {}).email || '', 'johnnygermano@grupodavid.com', 'josecarreno@grupodavid.com'], `FACTURA CON RUC ERRADO ${(selectedLocal || {}).name || ''}`, bodyHTML, null, null)
+          .catch(error => res.send(error));
+      }
+    }
+
+  });
+
+  console.log(`connect ${codeTerminal} - idApp`, listClient.id);
+  console.log('a user connected');
 });
 
 
 
 httpServer.listen(3200, async () => {
-    console.log('listening on *:3200');
+  console.log('listening on *:3200');
 });
