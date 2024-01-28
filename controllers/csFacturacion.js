74fb99531d17fc070f69483873feb7b9ce7387cb
acvsystem <36681777+acvsystem@users.noreply.github.com>
2024-01-28 13:05:39 -0500

    update

--- controllers/csFacturacion.js
+++ controllers/csFacturacion.js
@@ -68,8 +68,8 @@ class clsFacturacion {
             const workBook = XLSX.utils.book_new();
             XLSX.utils.book_append_sheet(workBook, workSheet, "attendance");
             const xlsFile = XLSX.write(workBook, { bookType: "xlsx", type: "buffer" });
-          /*  emailController.sendEmail('johnnygermano@grupodavid.com', `${(selectedLocal || {}).name} - FACTURAS FALTANTES EN SERVIDOR`, null, xlsFile, (selectedLocal || {}).name)
-                .catch(error => res.send(error));*/
+            emailController.sendEmail('johnnygermano@grupodavid.com', `${(selectedLocal || {}).name} - FACTURAS FALTANTES EN SERVIDOR`, null, xlsFile, (selectedLocal || {}).name)
+                .catch(error => res.send(error));
         }
 
         await pool.query(`UPDATE TB_TERMINAL_TIENDA SET VERIFICACION = true, CANT_COMPROBANTES = ${(dataNoFound || []).length} WHERE CODIGO_TERMINAL = '${codigoFront}'`);
