fa625def87af35ffb4f0bb939464e1fd99a65be3
acvsystem <36681777+acvsystem@users.noreply.github.com>
2024-03-18 17:03:12 -0500

    update user mysql

--- conections/conexMysql.js
+++ conections/conexMysql.js
@@ -2,7 +2,7 @@ import { createPool } from 'mysql2/promise';
 
 export const pool = createPool({
     host: '127.0.0.1',
-    user: 'dbapp',
+    user: 'dbserver',
     port: 3306,
     password: 'J4s0nd34d$$',
     database: 'BD_METASPERU',
