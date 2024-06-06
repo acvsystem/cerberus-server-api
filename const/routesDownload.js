218ea0fd8ea30cabb5ff8469b92214fe6aa0617a
acvsystem <36681777+acvsystem@users.noreply.github.com>
2024-06-06 10:09:08 -0500

    update

---
+++ const/routesDownload.js
@@ -0,0 +1,8 @@
+export const pathDownload = {
+    path: {
+        agente: "./download/agente/agnFront.py",
+        pluginDocument:"./download/pluginDocument/plugin.zip",
+        pluginSunat:"./download/pluginSunat/SUNAT.zip",
+        server: "./update/server.py"
+    }
+};
