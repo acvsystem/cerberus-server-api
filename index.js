c04bc8a2ac5982ac59358d8fff49519f3b3272cf
acvsystem <36681777+acvsystem@users.noreply.github.com>
2024-06-13 13:02:31 -0500

    update

--- index.js
+++ index.js
@@ -219,7 +219,7 @@ io.on('connection', async (socket) => {
 
   app.post("/frontRetail/search/stock", async (req, res) => {
     let codigoTienda = (((req || {}).body || [])[0] || {}).cCodigoTienda;
-
+    console.log('stock', codigoTienda);
     let dataResponse = [];
     let dataProcess = [];
 
