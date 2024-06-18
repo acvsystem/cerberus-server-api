3b47779af1bed19d672cc84a48593831b9b15a30
acvsystem <36681777+acvsystem@users.noreply.github.com>
2024-06-18 09:31:36 -0500

    update

--- controllers/csFacturacion.js
+++ controllers/csFacturacion.js
@@ -63,7 +63,7 @@ class clsFacturacion {
         let selectedLocal = tiendasList.find((data) => data.code == codigoFront);
         console.log(`${this.getDate()} - ${codigoFront} - ${(selectedLocal || {}).name} - Comprobantes enviados: ${(dataNoFound || []).length}`);
 
-        if ((dataNoFound || []).length >= 10) {
+        if ((dataNoFound || []).length > 0) {
             const workSheet = XLSX.utils.json_to_sheet((dataNoFound || []));
             const workBook = XLSX.utils.book_new();
             XLSX.utils.book_append_sheet(workBook, workSheet, "attendance");
