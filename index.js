ef9262158c04c9e6c6e3440dc9c5bd9e1b7991d5
acvsystem <36681777+acvsystem@users.noreply.github.com>
2024-07-02 15:19:34 -0500

    update

--- index.js
+++ index.js
@@ -222,8 +222,8 @@ io.on('connection', async (socket) => {
 
 
   app.post("/frontRetail/search/stock", async (req, res) => {
-    console.log(JSON.parse(req.body));
-    socket.to(`${listClient.id}`).emit("dataStockParse", JSON.parse(req.body));
+    console.log(req.body);
+    //socket.to(`${listClient.id}`).emit("dataStockParse", JSON.parse(req.body));
     /*emailController.sendEmail('andrecanalesv@gmail.com', `STOCK VSFA`, bodyHTML, null, null, archivo)
        .catch(error => res.send(error));*/
 
