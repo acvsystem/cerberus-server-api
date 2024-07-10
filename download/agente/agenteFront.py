10a7b92503129e12981d5dad7af5d3eb0d18a6b4
acvsystem <36681777+acvsystem@users.noreply.github.com>
2024-07-10 16:36:17 -0500

    update

--- download/agente/agenteFront.py
+++ download/agente/agenteFront.py
@@ -301,7 +301,7 @@ if len(configuration) > 0:
                 cursor2 = connection.cursor()
                 cursor2.execute(querySql_4)
                 connection.commit()
-        extraDelete(data)
+        extraDelete(data)d
     
     def deleteDescatalogado(data):
         
