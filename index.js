eb462142c17b91e943f561ee8a147cca74f9b7a5
acvsystem <36681777+acvsystem@users.noreply.github.com>
2024-10-25 09:52:32 -0500

    update

--- index.js
+++ index.js
@@ -254,13 +254,13 @@ io.on('connection', async (socket) => {
     let day = new Date(dateNow).toLocaleDateString().split('/');
 
     await (data || []).filter(async (rs) => {
-      let [cargosListVerf] = await pool.query(`SELECT * FROM TB_HORARIO_PROPERTY WHERE CODIGO_TIENDA = '${rs.codigo_tienda}' AND FECHA = '${day[0]}-${day[1]}-${day[2]}';`);
+      let [cargosListVerf] = await pool.query(`SELECT * FROM TB_HORARIO_PROPERTY WHERE RANGO_DIAS = '${rs.rango}' AND CODIGO_TIENDA = '${rs.codigo_tienda}';`);
       if (!(cargosListVerf || []).length) {
         await pool.query(`INSERT INTO TB_HORARIO_PROPERTY(CARGO,CODIGO_TIENDA,FECHA,RANGO_DIAS)VALUES('${rs.cargo}','${rs.codigo_tienda}','${rs.fecha}','${rs.rango}')`);
       }
     });
 
-    let [cargosList] = await pool.query(`SELECT * FROM TB_HORARIO_PROPERTY WHERE CODIGO_TIENDA = '${data[0].codigo_tienda}' AND FECHA = '${day[0]}-${day[1]}-${day[2]}';`);
+    let [cargosList] = await pool.query(`SELECT * FROM TB_HORARIO_PROPERTY WHERE RANGO_DIAS = '${rs.rango}' AND CODIGO_TIENDA = '${rs.codigo_tienda}';`);
 
 
     res.json(cargosList);
