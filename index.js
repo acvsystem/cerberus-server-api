97efffe3c5675f1e31e50b1ef96b052246d56219
acvsystem <36681777+acvsystem@users.noreply.github.com>
2024-11-16 11:13:24 -0500

    update

--- index.js
+++ index.js
@@ -289,7 +289,7 @@ io.on('connection', async (socket) => {
   });
 
   app.post("/planilla/FDM", async (req, res) => {
-    let response = req.body; 
+    let response = JSON.parse(req.body); 
     console.log(response);
    // var serverData = JSON.parse((response || {}).serverData);
    // var configuration = JSON.parse((response || {}).configuration);
