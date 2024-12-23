c219620a32f4978f1d4089a48bab150598137ab4
acvsystem <36681777+acvsystem@users.noreply.github.com>
2024-12-23 10:39:22 -0500

    UPDATE

--- index.js
+++ index.js
@@ -775,12 +775,12 @@ io.on('connection', async (socket) => {
           fecha: dt.fecha,
           hrx_acumulado: dt.hrx_acumulado,
           extra: dt.extra,
-          hrx_tomada: (arHrExtra)[0]['HR_EXTRA_TOMADA'] || 0,
-          hrx_sobrante: (arHrExtra)[0]['HR_EXTRA_SOBRANTE'] || 0,
-          estado: (arHrExtra)[0]['ESTADO'],
-          aprobado: arHrExtra[0]['APROBADO'] == 1 ? true : false,
-          seleccionado: arHrExtra[0]['SELECCIONADO'] == 1 ? true : false,
-          verify: arHrExtra[0]['SELECCIONADO'] == 1 ? true : false
+          hrx_tomada: ((arHrExtra || [])[0] || {})['HR_EXTRA_TOMADA'] || 0,
+          hrx_sobrante: ((arHrExtra || [])[0] || {})['HR_EXTRA_SOBRANTE'] || 0,
+          estado: ((arHrExtra || [])[0] || {})['ESTADO'],
+          aprobado: ((arHrExtra || [])[0] || {})['APROBADO'] == 1 ? true : false,
+          seleccionado: ((arHrExtra || [])[0] || {})['SELECCIONADO'] == 1 ? true : false,
+          verify: ((arHrExtra || [])[0] || {})['SELECCIONADO'] == 1 ? true : false
         });
       } else {
         data[i]['verify'] = false;
