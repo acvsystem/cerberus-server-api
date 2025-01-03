0ce7186aa8c86f21597a7cabacd7c763794bbd7a
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-01-03 11:56:15 -0500

    update

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ import securityRoutes from "./routes/security.routes.js";
 import recursosHumanosRoutes from "./routes/recursosHumanos.routes.js";
 import { prop as defaultResponse } from "./const/defaultResponse.js";
 import tokenController from './controllers/csToken.js';
+import fs from 'fs';
+
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, { cors: { origin: "*", methods: ["GET", "POST"], transports: ['websocket', 'polling'] } });
-const fs = require('fs');
+
 
 app.use(
   cors({
