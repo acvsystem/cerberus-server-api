3422e41b45ffed796e97e96e502a924cabdce391
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-01-09 12:26:40 -0500

    update

--- index.js
+++ index.js
@@ -772,13 +772,15 @@ io.on('connection', async (socket) => {
         });
 
         await pool.query(`SELECT * FROM TB_OBSERVACION WHERE ID_OBS_HORARIO = ${dth.id};`).then((requestObs) => {
-          arObservation.push("true");
-          ([requestObs] || []).filter(async (obs) => {
+
+          await ([requestObs] || []).filter(async (obs) => {
             response[index]['observacion'].push({ id: obs.ID_OBSERVACION, id_dia: obs.ID_OBS_DIAS, nombre_completo: obs.NOMBRE_COMPLETO, observacion: obs.OBSERVACION });
           });
+          arObservation.push("true");
         });
+
         console.log(arObservation);
-        if (requestSql.length - 1 == index) {
+        if (requestSql.length - 1 == index && arObservation.length == 4) {
           res.json(response);
         }
 
