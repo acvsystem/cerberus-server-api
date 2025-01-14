a0dc11567f1025d7b0acc21bcbc0f65353e160d1
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-01-14 17:38:03 -0500

    update

--- controllers/csPapeletas.js
+++ controllers/csPapeletas.js
@@ -76,7 +76,7 @@ export const regHorasExtras = async (req, res) => {
                         codigo_papeleta: (hrx || {}).codigo_papeleta,
                         fecha: (hrx || {}).fecha,
                         hrx_acumulado: (hrx || {}).hrx_acumulado,
-                        extra: ((arHrExtra || [])[0] || {})['HR_EXTRA_SOLICITADO'] == (hrx || {}).extra ? (hrx || {}).extra : ((arHrExtra || [])[0] || {})['HR_EXTRA_SOLICITADO'],
+                        extra: ((arHrExtra || [])[0] || {})['HR_EXTRA_ACUMULADO'] == (hrx || {}).extra ? (hrx || {}).extra : ((arHrExtra || [])[0] || {})['HR_EXTRA_ACUMULADO'],
                         hrx_solicitado: ((arHrExtra || [])[0] || {})['HR_EXTRA_SOLICITADO'] || '00:00',
                         hrx_sobrante: ((arHrExtra || [])[0] || {})['HR_EXTRA_SOBRANTE'] || '00:00',
                         estado: (hrx || {}).estado || ((arHrExtra || [])[0] || {})['ESTADO'],
