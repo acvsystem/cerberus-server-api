3529f52795ffb9efa7b3379222739c42647673d2
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-01-17 15:13:55 -0500

    UPDATE

--- index.js
+++ index.js
@@ -842,7 +842,7 @@ io.on('connection', async (socket) => {
         AND RANGO_DIAS = '${(hrr || {}).rango}' 
         AND CARGO = '${(hrr || {}).cargo}'
         AND CODIGO_TIENDA = '${(hrr || {}).codigo_tienda}';`).then((rs) => {
-          console.log(rs);
+          console.log([rs]);
         });
       });
     });
