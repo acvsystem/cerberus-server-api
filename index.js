8619cfaa7c9a82ec883b35b8b7a5a2547e69f0e2
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-01-17 14:56:15 -0500

    update

--- index.js
+++ index.js
@@ -838,7 +838,7 @@ io.on('connection', async (socket) => {
     let arHorario = req.body;
     (arHorario || []).filter((hrr) => {
       let id_cargo_horario = 0;
-      pool.query(`CALL SP_HORARIO_PROPERTY('${(hrr || {}).fecha}','${(hrr || {}).rango}','${(hrr || {}).cargo}','${(hrr || {}).codigo_tienda}',@output);select @output;`).then((rs) => {
+      pool.query(`CALL SP_HORARIO_PROPERTY('${(hrr || {}).fecha}','${(hrr || {}).rango}','${(hrr || {}).cargo}','${(hrr || {}).codigo_tienda}',@output);`).then((rs) => {
         //console.log(id_cargo_horario);
         console.log(rs);
       });
