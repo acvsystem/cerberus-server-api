91622410e705c8bf19ea1a1da4fd51231887c997
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-02-03 18:35:08 -0500

    update

--- index.js
+++ index.js
@@ -1437,14 +1437,15 @@ io.on('connection', async (socket) => {
 
 
   const storage = multer.diskStorage({
-
-    destination: './driveCloud/EMBARQUES/',
-    filename: (req, file, cb) => {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-      const ext = path.extname(file.originalname);
-      cb(null, file.fieldname + '-' + uniqueSuffix + ext);
+    
+    destination: function (req, file, callback) {
+      callback(null, './driveCloud/EMBARQUES/');
+    },
+    // Sets file(s) to be saved in uploads folder in same directory
+    filename: function (req, file, callback) {
+      callback(null, file.originalname);
     }
-
+    // Sets saved filename(s) to be original filename(s)
   })
 
   // Set saved storage options:
