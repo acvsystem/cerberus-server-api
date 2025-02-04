2cc63985d50c7498b8634f02c1811946e56252e0
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-02-04 17:26:47 -0500

    update

--- index.js
+++ index.js
@@ -1435,42 +1435,7 @@ io.on('connection', async (socket) => {
     res.download(fileLocation, file);
   });
 
-  const multi_upload = multer({
-    storage,// 1MB
-    fileFilter: (req, file, cb) => {
-      if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
-        cb(null, './driveCloud/EMBARQUES/');
-      } else {
-        cb(null, false);
-        const err = new Error('Only .png, .jpg and .jpeg format allowed!')
-        err.name = 'ExtensionError'
-        return cb(err);
-      }
-    },
-  }).array('file', 10)
 
-  app.post('/upload/driveCloud', (req, res) => {
-    multi_upload(req, res, function (err) {
-      if (err instanceof multer.MulterError) {
-        // A Multer error occurred when uploading.
-        res.status(500).send({ error: { message: `Multer uploading error: ${err.message}` } }).end();
-        return;
-      } else if (err) {
-        // An unknown error occurred when uploading.
-        if (err.name == 'ExtensionError') {
-          res.status(413).send({ error: { message: err.message } }).end();
-        } else {
-          res.status(500).send({ error: { message: `unknown uploading error: ${err.message}` } }).end();
-        }
-        return;
-      }
-
-      // Everything went fine.
-      // show file `req.files`
-      // show body `req.body`
-      res.status(200).end('Your files uploaded.');
-    })
-  });
 
   app.post('/oneListDirectory', async (req, res) => {
     let arDirectory = [];
@@ -1637,6 +1602,42 @@ io.on('connection', async (socket) => {
   console.log('a user connected');
 });
 
+const multi_upload = multer({
+  storage,// 1MB
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+      cb(null, './driveCloud/EMBARQUES/');
+    } else {
+      cb(null, false);
+      const err = new Error('Only .png, .jpg and .jpeg format allowed!')
+      err.name = 'ExtensionError'
+      return cb(err);
+    }
+  },
+}).array('file', 10)
+
+app.post('/upload/driveCloud', (req, res) => {
+  multi_upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      // A Multer error occurred when uploading.
+      res.status(500).send({ error: { message: `Multer uploading error: ${err.message}` } }).end();
+      return;
+    } else if (err) {
+      // An unknown error occurred when uploading.
+      if (err.name == 'ExtensionError') {
+        res.status(413).send({ error: { message: err.message } }).end();
+      } else {
+        res.status(500).send({ error: { message: `unknown uploading error: ${err.message}` } }).end();
+      }
+      return;
+    }
+
+    // Everything went fine.
+    // show file `req.files`
+    // show body `req.body`
+    res.status(200).end('Your files uploaded.');
+  })
+});
 
 
 httpServer.listen(3700, async () => {
