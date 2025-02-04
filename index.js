793dde220b4c6cdb6cb56e7ee2a84474b010ac82
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-02-04 15:57:23 -0500

    update

--- index.js
+++ index.js
@@ -1622,6 +1622,6 @@ io.on('connection', async (socket) => {
 
 
 
-httpServer.listen(3800, async () => {
-  console.log('listening on *:3800');
+httpServer.listen(3700, async () => {
+  console.log('listening on *:3700');
 });
