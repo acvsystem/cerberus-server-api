f3feaeb1b0d7cdec1f8820e9d52d860bd0e5dec3
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-02-04 17:10:16 -0500

    update

--- index.js
+++ index.js
@@ -28,11 +28,6 @@ app.use(
   })
 );
 
-app.get("/test", async (req, res) => {
-  res.writeHead(200);
-  res.end("My first server!");
-});
-
 app.use(bodyParser.json({ limit: "1000000mb" }));
 app.use(bodyParser.urlencoded({ limit: "1000000mb", extended: true }));
 
@@ -1453,11 +1448,41 @@ io.on('connection', async (socket) => {
     // Sets saved filename(s) to be original filename(s)
   })
 
-  // Set saved storage options:
-  const upload = multer({ storage: storage })
+  const multi_upload = multer({
+    storage,// 1MB
+    fileFilter: (req, file, cb) => {
+      if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+        cb(null, true);
+      } else {
+        cb(null, false);
+        const err = new Error('Only .png, .jpg and .jpeg format allowed!')
+        err.name = 'ExtensionError'
+        return cb(err);
+      }
+    },
+  }).array('uploadedImages', 10)
 
-  app.post('/upload/driveCloud', upload.single('file'), (req, res) => {
-    res.json({ message: 'Upload success' });
+  app.post('/upload/driveCloud', (req, res) => {
+    multi_upload(req, res, function (err) {
+      if (err instanceof multer.MulterError) {
+        // A Multer error occurred when uploading.
+        res.status(500).send({ error: { message: `Multer uploading error: ${err.message}` } }).end();
+        return;
+      } else if (err) {
+        // An unknown error occurred when uploading.
+        if (err.name == 'ExtensionError') {
+          res.status(413).send({ error: { message: err.message } }).end();
+        } else {
+          res.status(500).send({ error: { message: `unknown uploading error: ${err.message}` } }).end();
+        }
+        return;
+      }
+
+      // Everything went fine.
+      // show file `req.files`
+      // show body `req.body`
+      res.status(200).end('Your files uploaded.');
+    })
   });
 
   app.post('/oneListDirectory', async (req, res) => {
