62edd664670b29262a09258da38cd9657760eee4
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-02-17 14:51:44 -0500

    update

--- index.js
+++ index.js
@@ -1477,7 +1477,7 @@ io.on('connection', async (socket) => {
 
   const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      console.log(__dirname);
+      
       cb(null, './driveCloud/EMBARQUES/');
     },
     filename: function (req, file, cb) {
