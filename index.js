20cba7bfea0966f7e650f454b85ea3a3f608833a
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-02-18 11:56:48 -0500

    upodate

--- index.js
+++ index.js
@@ -1539,7 +1539,7 @@ io.on('connection', async (socket) => {
 
   app.post('/sunat-notification', async (req, res) => {
 
-    let arrDocumento = (((req || []).body || [])[0] || {});
+    let arrDocumento = (req || []).body || [];
     console.log((req || []).body);
     let tiendasList = [
       { code: '7A', name: 'BBW JOCKEY', email: 'bbwjockeyplaza@metasperu.com' },
@@ -1563,119 +1563,122 @@ io.on('connection', async (socket) => {
       { code: '9P', name: 'VS MALL PLAZA', email: 'vsmallplazatrujillo@metasperu.com' },
       { code: '7I', name: 'BBW MALL PLAZA', email: 'bbwmallplazatrujillo@metasperu.com' }
     ];
-    
-    if (((arrDocumento || {}).ESTADO_SUNAT || "").trim() == "ACEPTADO" || ((arrDocumento || {}).ESTADO_SUNAT || "").trim() == "GENERADO") {
-      
-      let [verifyDocument] = await pool.query(`SELECT * FROM TB_DOCUMENTOS_ERROR_SUNAT WHERE CODIGO_DOCUMENTO = '${(arrDocumento || {}).CODIGO_DOCUMENTO}';`);
- 
-      let isEmailEnvio = ((verifyDocument || [])[0] || {}).ENVIO_EMAIL || 'false';
-      console.log('verifyDocument', (verifyDocument || []).length);
 
-      if (!(verifyDocument || []).length) {
-        await pool.query(`INSERT INTO TB_DOCUMENTOS_ERROR_SUNAT(CODIGO_DOCUMENTO,NRO_CORRELATIVO,NOM_ADQUIRIENTE,NRO_DOCUMENTO,TIPO_DOCUMENTO_ADQUIRIENTE,OBSERVACION,ESTADO_SUNAT,ESTADO_COMPROBANTE,CODIGO_ERROR_SUNAT,ENVIO_EMAIL,FECHA_EMISION)
-                                VALUES(${(arrDocumento || {}).CODIGO_DOCUMENTO},
-                                '${(arrDocumento || {}).NRO_CORRELATIVO}',
-                                '${(arrDocumento || {}).NOM_ADQUIRIENTE}',
-                                '${(arrDocumento || {}).NRO_DOCUMENTO}',
-                                '',
-                                '',
-                                '${(arrDocumento || {}).ESTADO_SUNAT}',
-                                '${(arrDocumento || {}).ESTADO_COMPROBANTE}',
-                                '${(arrDocumento || {}).CODIGO_ERROR_SUNAT}',
-                                'false',
-                                '${(arrDocumento || {}).FECHA_EMISION}');`);
+    (arrDocumento || []).filer(async (doc) => {
+      if (((doc || {}).ESTADO_SUNAT || "").trim() == "ACEPTADO" || ((doc || {}).ESTADO_SUNAT || "").trim() == "GENERADO") {
 
-        res.send('RECEPCION EXITOSA..!!');
-      } else {
-        await pool.query(`UPDATE TB_DOCUMENTOS_ERROR_SUNAT SET
-                                NOM_ADQUIRIENTE ='${(arrDocumento || {}).NOM_ADQUIRIENTE}',
-                                NRO_DOCUMENTO = '${(arrDocumento || {}).NRO_DOCUMENTO}',
-                                OBSERVACION = '',
-                                ESTADO_SUNAT = '${(arrDocumento || {}).ESTADO_SUNAT}',
-                                ESTADO_COMPROBANTE = '${(arrDocumento || {}).ESTADO_COMPROBANTE}',
-                                CODIGO_ERROR_SUNAT = '${(arrDocumento || {}).CODIGO_ERROR_SUNAT}' WHERE CODIGO_DOCUMENTO = ${(arrDocumento || {}).CODIGO_DOCUMENTO};`);
-        res.send('RECEPCION EXITOSA..!!');
-      }
+        let [verifyDocument] = await pool.query(`SELECT * FROM TB_DOCUMENTOS_ERROR_SUNAT WHERE CODIGO_DOCUMENTO = '${(doc || {}).CODIGO_DOCUMENTO}';`);
 
-      let [documentList] = await pool.query(`SELECT * FROM TB_DOCUMENTOS_ERROR_SUNAT;`);
-      socket.to(`${listClient.id}`).emit("sendNotificationSunat", documentList);
+        let isEmailEnvio = ((verifyDocument || [])[0] || {}).ENVIO_EMAIL || 'false';
 
-      /*let errDocument = [
-          {
-              'ID.FACTURA': (arrDocumento || {}).CODIGO_DOCUMENTO,
-              'NUM.FACTURA': (arrDocumento || {}).NRO_CORRELATIVO,
-              'FEC.EMISION': (arrDocumento || {}).FECHA_EMISION,
-              'NOM.CLIENTE': (arrDocumento || {}).NOM_ADQUIRIENTE,
-              'NUM.DOCUMENTO': (arrDocumento || {}).NRO_DOCUMENTO,
+        if (!(verifyDocument || []).length) {
+          await pool.query(`INSERT INTO TB_DOCUMENTOS_ERROR_SUNAT(CODIGO_DOCUMENTO,NRO_CORRELATIVO,NOM_ADQUIRIENTE,NRO_DOCUMENTO,TIPO_DOCUMENTO_ADQUIRIENTE,OBSERVACION,ESTADO_SUNAT,ESTADO_COMPROBANTE,CODIGO_ERROR_SUNAT,ENVIO_EMAIL,FECHA_EMISION)
+                                  VALUES(${(doc || {}).CODIGO_DOCUMENTO},
+                                  '${(doc || {}).NRO_CORRELATIVO}',
+                                  '${(doc || {}).NOM_ADQUIRIENTE}',
+                                  '${(doc || {}).NRO_DOCUMENTO}',
+                                  '',
+                                  '',
+                                  '${(doc || {}).ESTADO_SUNAT}',
+                                  '${(doc || {}).ESTADO_COMPROBANTE}',
+                                  '${(doc || {}).CODIGO_ERROR_SUNAT}',
+                                  'false',
+                                  '${(doc || {}).FECHA_EMISION}');`);
+
+          res.send('RECEPCION EXITOSA..!!');
+        } else {
+          await pool.query(`UPDATE TB_DOCUMENTOS_ERROR_SUNAT SET
+                                  NOM_ADQUIRIENTE ='${(doc || {}).NOM_ADQUIRIENTE}',
+                                  NRO_DOCUMENTO = '${(doc || {}).NRO_DOCUMENTO}',
+                                  OBSERVACION = '',
+                                  ESTADO_SUNAT = '${(doc || {}).ESTADO_SUNAT}',
+                                  ESTADO_COMPROBANTE = '${(doc || {}).ESTADO_COMPROBANTE}',
+                                  CODIGO_ERROR_SUNAT = '${(doc || {}).CODIGO_ERROR_SUNAT}' WHERE CODIGO_DOCUMENTO = ${(doc || {}).CODIGO_DOCUMENTO};`);
+          res.send('RECEPCION EXITOSA..!!');
+        }
+
+        let [documentList] = await pool.query(`SELECT * FROM TB_DOCUMENTOS_ERROR_SUNAT;`);
+        socket.to(`${listClient.id}`).emit("sendNotificationSunat", documentList);
+
+        /*let errDocument = [
+            {
+                'ID.FACTURA': (arrDocumento || {}).CODIGO_DOCUMENTO,
+                'NUM.FACTURA': (arrDocumento || {}).NRO_CORRELATIVO,
+                'FEC.EMISION': (arrDocumento || {}).FECHA_EMISION,
+                'NOM.CLIENTE': (arrDocumento || {}).NOM_ADQUIRIENTE,
+                'NUM.DOCUMENTO': (arrDocumento || {}).NRO_DOCUMENTO,
+            }
+        ]*/
+
+        var bodyHTML = `<p>Buenos días, adjunto los datos de una factura emitida con numero de RUC errado.</p> 
+          
+              <p>Lamentablemente no han cumplido con los procesos y métodos de validación que se les han proporcionado.</p>  
+              
+              <p>Quedo atento de la persona responsable de emitir dicha factura.</p>  
+              
+              <p>Realizar la NC con anticipo y/o vale.  Si tienen alguna inquietud me dejan saber.</p>
+              
+              <p>Saludos.
+          
+              <p><strong>Datos de factura emitida:</strong></p>
+              
+              <table align="left" cellspacing="0">
+                  <thead>
+                      <tr>
+                          <th style="border: 1px solid #9E9E9E;border-right:0px" width="110px">ID.FACTURA</th>
+                          <th style="border: 1px solid #9E9E9E;border-right:0px" width="110px">NUM.FACTURA</th>
+                          <th style="border: 1px solid #9E9E9E;border-right:0px" width="110px">FEC.EMISION</th>
+                          <th style="border: 1px solid #9E9E9E;border-right:0px" width="200px">NOM.CLIENTE</th>
+                          <th style="border: 1px solid #9E9E9E" width="140px">NUM.DOCUMENTO</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      <tr>
+                          <td style="border: 1px solid #9E9E9E;border-top:0px;text-align:center;border-right:0px">${(doc || {}).CODIGO_DOCUMENTO}</td>
+                          <td style="border: 1px solid #9E9E9E;border-top:0px;text-align:center">${(doc || {}).NRO_CORRELATIVO}</td>
+                          <td style="border: 1px solid #9E9E9E;border-top:0px;text-align:center;border-right:0px">${(doc || {}).FECHA_EMISION}</td>
+                          <td style="border: 1px solid #9E9E9E;border-top:0px;text-align:center;border-right:0px">${(doc || {}).NOM_ADQUIRIENTE}</td>
+                          <td style="border: 1px solid #9E9E9E;border-top:0px;text-align:center">${(doc || {}).NRO_DOCUMENTO}</td>
+                      </tr>
+                  </tbody>
+              </table>`;
+
+
+
+        let serie = ((doc || {}).NRO_CORRELATIVO || "").split('-')[0];
+
+        var codigo = serie.substr(1, 3);
+        var selectedLocal = {};
+        var count = 0;
+
+
+        while (count <= 1) {
+
+          if (count == 1) {
+            codigo = serie.substr(1, 2);
           }
-      ]*/
 
-      var bodyHTML = `<p>Buenos días, adjunto los datos de una factura emitida con numero de RUC errado.</p> 
-        
-            <p>Lamentablemente no han cumplido con los procesos y métodos de validación que se les han proporcionado.</p>  
-            
-            <p>Quedo atento de la persona responsable de emitir dicha factura.</p>  
-            
-            <p>Realizar la NC con anticipo y/o vale.  Si tienen alguna inquietud me dejan saber.</p>
-            
-            <p>Saludos.
-        
-            <p><strong>Datos de factura emitida:</strong></p>
-            
-            <table align="left" cellspacing="0">
-                <thead>
-                    <tr>
-                        <th style="border: 1px solid #9E9E9E;border-right:0px" width="110px">ID.FACTURA</th>
-                        <th style="border: 1px solid #9E9E9E;border-right:0px" width="110px">NUM.FACTURA</th>
-                        <th style="border: 1px solid #9E9E9E;border-right:0px" width="110px">FEC.EMISION</th>
-                        <th style="border: 1px solid #9E9E9E;border-right:0px" width="200px">NOM.CLIENTE</th>
-                        <th style="border: 1px solid #9E9E9E" width="140px">NUM.DOCUMENTO</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td style="border: 1px solid #9E9E9E;border-top:0px;text-align:center;border-right:0px">${(arrDocumento || {}).CODIGO_DOCUMENTO}</td>
-                        <td style="border: 1px solid #9E9E9E;border-top:0px;text-align:center">${(arrDocumento || {}).NRO_CORRELATIVO}</td>
-                        <td style="border: 1px solid #9E9E9E;border-top:0px;text-align:center;border-right:0px">${(arrDocumento || {}).FECHA_EMISION}</td>
-                        <td style="border: 1px solid #9E9E9E;border-top:0px;text-align:center;border-right:0px">${(arrDocumento || {}).NOM_ADQUIRIENTE}</td>
-                        <td style="border: 1px solid #9E9E9E;border-top:0px;text-align:center">${(arrDocumento || {}).NRO_DOCUMENTO}</td>
-                    </tr>
-                </tbody>
-            </table>`;
+          selectedLocal = tiendasList.find((data) => data.code == codigo) || {};
+          if (Object.keys(selectedLocal).length) {
+            count = 1;
+          }
 
-
-
-      let serie = ((arrDocumento || {}).NRO_CORRELATIVO || "").split('-')[0];
-
-      var codigo = serie.substr(1, 3);
-      var selectedLocal = {};
-      var count = 0;
-
-
-      while (count <= 1) {
-
-        if (count == 1) {
-          codigo = serie.substr(1, 2);
+          count++;
         }
 
-        selectedLocal = tiendasList.find((data) => data.code == codigo) || {};
-        if (Object.keys(selectedLocal).length) {
-          count = 1;
+        if (Object.keys(selectedLocal).length && isEmailEnvio != 'true') {
+          console.log("sunat:codigo_tienda", codigo);
+          console.log("sunat:tienda", selectedLocal);
+
+          await pool.query(`UPDATE TB_DOCUMENTOS_ERROR_SUNAT SET ENVIO_EMAIL ='true' WHERE CODIGO_DOCUMENTO = ${(doc || {}).CODIGO_DOCUMENTO};`);
+          /*
+                  emailController.sendEmail([(selectedLocal || {}).email || '', 'johnnygermano@metasperu.com', 'josecarreno@metasperu.com'], `FACTURA CON RUC ERRADO ${(selectedLocal || {}).name || ''}`, bodyHTML, null, null)
+                    .catch(error => res.send(error));*/
         }
-
-        count++;
       }
+    });
 
-      if (Object.keys(selectedLocal).length && isEmailEnvio != 'true') {
-        console.log("sunat:codigo_tienda", codigo);
-        console.log("sunat:tienda", selectedLocal);
 
-        await pool.query(`UPDATE TB_DOCUMENTOS_ERROR_SUNAT SET ENVIO_EMAIL ='true' WHERE CODIGO_DOCUMENTO = ${(arrDocumento || {}).CODIGO_DOCUMENTO};`);
-/*
-        emailController.sendEmail([(selectedLocal || {}).email || '', 'johnnygermano@metasperu.com', 'josecarreno@metasperu.com'], `FACTURA CON RUC ERRADO ${(selectedLocal || {}).name || ''}`, bodyHTML, null, null)
-          .catch(error => res.send(error));*/
-      }
-    }
 
   });
 
