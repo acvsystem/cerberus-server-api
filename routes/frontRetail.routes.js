b6b35ea694a461ea359783ad5336ebba9091518f
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-03-04 09:38:24 -0500

    update

--- routes/frontRetail.routes.js
+++ routes/frontRetail.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 const router = Router();
-import { onStock, onAgenteConfigList } from '../controllers/csFrontRetail.js';
+import { onStock, onAgenteConfigList, onRegistrarTienda } from '../controllers/csFrontRetail.js';
 
 router.post('/search/configuration/agente', onAgenteConfigList);
+router.post('/add/tienda', onRegistrarTienda);
 router.post('/search/stock', onStock);
 
 const frontRetailRoutes = router;
