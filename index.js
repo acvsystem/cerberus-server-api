0e8b641b91c1201897b1bb0b304362a36b4cd926
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-03-27 11:09:55 -0500

    update

--- index.js
+++ index.js
@@ -455,7 +455,7 @@ io.on('connection', async (socket) => {
 
     (response || []).filter(async (mc, i) => {
 
-      let date = new Date((mc || {}).fecha).split(' ')[0].split('/');
+      let date = new Date((((mc || {}).fecha) || "").split(' ')[0] || "").split('/');
       let parseDate = `${date[1]}-${date[0]}-${date[2]}`;
       console.log(parseDate,(mc || {}).documento);
       await pool.query(`SELECT TB_DIAS_TRABAJO.CODIGO_TIENDA,TB_DIAS_TRABAJO.NOMBRE_COMPLETO,TB_DIAS_TRABAJO.NUMERO_DOCUMENTO,TB_RANGO_HORA.RANGO_HORA,TB_DIAS_HORARIO.FECHA_NUMBER FROM TB_DIAS_TRABAJO INNER JOIN TB_RANGO_HORA ON TB_RANGO_HORA.ID_RANGO_HORA = TB_DIAS_TRABAJO.ID_TRB_RANGO_HORA INNER JOIN TB_DIAS_HORARIO ON TB_DIAS_HORARIO.ID_DIAS = TB_DIAS_TRABAJO.ID_TRB_DIAS WHERE FECHA_NUMBER = '${parseDate}' AND NUMERO_DOCUMENTO = '${(mc || {}).documento}';`).then(([rs]) => {
