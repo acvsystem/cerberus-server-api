b99e6584adc2120b8fd14bce73be47914652bb3e
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-04-01 11:49:29 -0500

    update

--- controllers/csFacturacion.js
+++ controllers/csFacturacion.js
@@ -110,7 +110,7 @@ class clsFacturacion {
 
                 (dataBk || []).filter((data, j) => {
                     var cpParse = (data || {}).cmpNumero;
-                    console.log(toString(cpParse || '').split('-')[0].substring(3, 4) + (toString(cpParse || '').split('-')[0].substring(0, 3)));
+                   // console.log(toString(cpParse || '').split('-')[0].substring(3, 4) + (toString(cpParse || '').split('-')[0].substring(0, 3)));
                     if (!(paseDataList || []).includes(cpParse)) {
                         console.log("verificacionCoeData", cpParse);
                         (dataNoFound || []).push({
