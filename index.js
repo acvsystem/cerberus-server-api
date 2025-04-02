1cc8adbd60d452fa993fe9fe40e63501ffe144c8
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-04-02 11:47:03 -0500

    update

--- index.js
+++ index.js
@@ -388,7 +388,7 @@ io.on('connection', async (socket) => {
   });
 
   app.post("/comparacion/bdTienda", async (req, res) => {
-    io.timeout(30000).emit("comparacionServer", 'DATA', async (err, response) => {
+    io.timeout(20000).emit("comparacionServer", 'DATA', async (err, response) => {
       let dataPlugin = (response || []).find((r) => typeof r != 'undefined');
       facturacionController.verificacionCoeData((dataPlugin || {}).DATA).then((dataResponse) => {
         res.json({ data: dataResponse });
