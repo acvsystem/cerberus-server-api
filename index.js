46b2f5833b297dc6499cd48111a439fb97f7ca95
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-05-12 12:07:20 -0500

    update

--- index.js
+++ index.js
@@ -1681,7 +1681,7 @@ io.on('connection', async (socket) => {
 
   app.post("/frontRetail/search/stock", async (req, res) => {
     let data = ((req || {}).body || []);
-    console.log(((data || [])[0] || {})['socketID'],((data || [])[0] || {})['cCodigoTienda']);
+    console.log(((data || [])[0] || {})['socketID'], ((data || [])[0] || {})['cCodigoTienda']);
     socket.to(`${((data || [])[0] || {})['socketID']}`).emit("dataStockParse", req.body);
 
     res.json({ mensaje: 'Archivo recibido con éxito' });
@@ -1702,13 +1702,9 @@ io.on('connection', async (socket) => {
 
       await pool.query(`SELECT TB_DIAS_TRABAJO.CODIGO_TIENDA,TB_DIAS_TRABAJO.NOMBRE_COMPLETO,TB_DIAS_TRABAJO.NUMERO_DOCUMENTO,TB_RANGO_HORA.RANGO_HORA,TB_DIAS_HORARIO.FECHA_NUMBER FROM TB_DIAS_TRABAJO INNER JOIN TB_RANGO_HORA ON TB_RANGO_HORA.ID_RANGO_HORA = TB_DIAS_TRABAJO.ID_TRB_RANGO_HORA INNER JOIN TB_DIAS_HORARIO ON TB_DIAS_HORARIO.ID_DIAS = TB_DIAS_TRABAJO.ID_TRB_DIAS WHERE FECHA_NUMBER = '${parseDate}' AND NUMERO_DOCUMENTO = '${(huellero || {}).nroDocumento}';`).then(([rs]) => {
 
-        if ((huellero || {}).nroDocumento == '74582231') {
-          console.log(((rs || [])[0] || {})['RANGO_HORA'] || "");
-        }
-
-        
-        ((dataServGeneral || [])[i] || {})['rango_horario'] = ((rs || [])[0] || {})['RANGO_HORA'] || "";
-        ((dataServGeneral || [])[i] || {})['isTardanza'] = false;
+        let indexR = dataServGeneral.findIndex((dt) => dt.dia == parseDate);
+        ((dataServGeneral || [])[indexR] || {})['rango_horario'] = ((rs || [])[0] || {})['RANGO_HORA'] || "";
+        ((dataServGeneral || [])[indexR] || {})['isTardanza'] = false;
 
         if (dataServGeneral.length - 1 == i) {
           console.log("dataServGeneral", dataServGeneral.length);
