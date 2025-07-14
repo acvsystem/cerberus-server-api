2ac8f10430ee413b439c269b86341e88ec87ddf2
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-07-14 11:40:36 -0500

    update

--- index.js
+++ index.js
@@ -842,8 +842,9 @@ io.on('connection', async (socket) => {
             console.log(`SELECT TB_DIAS_TRABAJO.CODIGO_TIENDA,TB_DIAS_TRABAJO.NOMBRE_COMPLETO,TB_DIAS_TRABAJO.NUMERO_DOCUMENTO,TB_RANGO_HORA.RANGO_HORA,TB_DIAS_HORARIO.FECHA_NUMBER FROM TB_DIAS_TRABAJO INNER JOIN TB_RANGO_HORA ON TB_RANGO_HORA.ID_RANGO_HORA = TB_DIAS_TRABAJO.ID_TRB_RANGO_HORA INNER JOIN TB_DIAS_HORARIO ON TB_DIAS_HORARIO.ID_DIAS = TB_DIAS_TRABAJO.ID_TRB_DIAS WHERE FECHA_NUMBER = '${parseDate}' AND NUMERO_DOCUMENTO = '${mc.documento}';`);
           }
 
-          ((response || [])[i] || {})['rango_horario'] = ((rs || [])[0] || {})['RANGO_HORA'] || "";
-          ((response || [])[i] || {})['isTardanza'] = false;
+          let indexRg = response.findIndex((rsInx)=>rsInx.documento == mc.documento && rsInx.checkinout == mc.checkinout);
+          ((response || [])[indexRg] || {})['rango_horario'] = ((rs || [])[0] || {})['RANGO_HORA'] || "";
+          ((response || [])[indexRg] || {})['isTardanza'] = false;
 
           if (response.length - 1 == i) {
             setTimeout(() => {
