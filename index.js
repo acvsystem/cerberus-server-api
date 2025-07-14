0a7d958923f9e2e68c3f90b18a3678fa64a16961
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-07-14 11:54:48 -0500

    update

--- index.js
+++ index.js
@@ -836,15 +836,20 @@ io.on('connection', async (socket) => {
       let parseDate = `${date[0]}-${parseInt(date[1])}-${date[2]}`;
       console.log(parseDate);
       if (date[2] == '2025') {
-        pool.query(`SELECT TB_DIAS_TRABAJO.CODIGO_TIENDA,TB_DIAS_TRABAJO.NOMBRE_COMPLETO,TB_DIAS_TRABAJO.NUMERO_DOCUMENTO,TB_RANGO_HORA.RANGO_HORA,TB_DIAS_HORARIO.FECHA_NUMBER FROM TB_DIAS_TRABAJO INNER JOIN TB_RANGO_HORA ON TB_RANGO_HORA.ID_RANGO_HORA = TB_DIAS_TRABAJO.ID_TRB_RANGO_HORA INNER JOIN TB_DIAS_HORARIO ON TB_DIAS_HORARIO.ID_DIAS = TB_DIAS_TRABAJO.ID_TRB_DIAS WHERE FECHA_NUMBER = '${parseDate}' AND NUMERO_DOCUMENTO = '${mc.documento}';`).then(([rs]) => {
+        pool.query(`SELECT TB_DIAS_TRABAJO.CODIGO_TIENDA,TB_DIAS_TRABAJO.NOMBRE_COMPLETO,TB_DIAS_TRABAJO.NUMERO_DOCUMENTO,TB_RANGO_HORA.RANGO_HORA,
+          TB_DIAS_HORARIO.FECHA_NUMBER FROM TB_DIAS_TRABAJO INNER JOIN TB_RANGO_HORA ON TB_RANGO_HORA.ID_RANGO_HORA = TB_DIAS_TRABAJO.ID_TRB_RANGO_HORA 
+          INNER JOIN TB_DIAS_HORARIO ON TB_DIAS_HORARIO.ID_DIAS = TB_DIAS_TRABAJO.ID_TRB_DIAS WHERE FECHA_NUMBER = '${parseDate}' AND NUMERO_DOCUMENTO = '${mc.documento}';`).then(([rs]) => {
+
 
           if (parseDate == '30-6-2025' && mc.documento == '76542350') {
             console.log(`SELECT TB_DIAS_TRABAJO.CODIGO_TIENDA,TB_DIAS_TRABAJO.NOMBRE_COMPLETO,TB_DIAS_TRABAJO.NUMERO_DOCUMENTO,TB_RANGO_HORA.RANGO_HORA,TB_DIAS_HORARIO.FECHA_NUMBER FROM TB_DIAS_TRABAJO INNER JOIN TB_RANGO_HORA ON TB_RANGO_HORA.ID_RANGO_HORA = TB_DIAS_TRABAJO.ID_TRB_RANGO_HORA INNER JOIN TB_DIAS_HORARIO ON TB_DIAS_HORARIO.ID_DIAS = TB_DIAS_TRABAJO.ID_TRB_DIAS WHERE FECHA_NUMBER = '${parseDate}' AND NUMERO_DOCUMENTO = '${mc.documento}';`);
+            console.log(((rs || [])[0] || {})['RANGO_HORA']);
           }
 
-          let indexRg = response.findIndex((rsInx)=>rsInx.documento == mc.documento && rsInx.checkinout == mc.checkinout);
-          ((response || [])[indexRg] || {})['rango_horario'] = ((rs || [])[0] || {})['RANGO_HORA'] || "";
-          ((response || [])[indexRg] || {})['isTardanza'] = false;
+          let indexRg = response.findIndex((rsInx) => rsInx.documento == mc.documento && rsInx.checkinout == mc.checkinout);
+
+          ((response || [])[i] || {})['rango_horario'] = ((rs || [])[0] || {})['RANGO_HORA'] || "";
+          ((response || [])[i] || {})['isTardanza'] = false;
 
           if (response.length - 1 == i) {
             setTimeout(() => {
