dfc40af48f0dcdecf26138e588f699fd684b971e
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-07-14 12:26:55 -0500

    update

--- index.js
+++ index.js
@@ -848,8 +848,8 @@ io.on('connection', async (socket) => {
         if (i >= 0) {
           onConsultarHorarioOficina(i, parseDate, mc.documento).then(([responseHorario]) => {
             console.log(responseHorario);
-            ((response || [])[(responseHorario || [])[0].index] || {})['rango_horario'] = (responseHorario || [])[0].horario || "";
-            ((response || [])[(responseHorario || [])[0].index] || {})['isTardanza'] = false;
+          //  ((response || [])[(responseHorario || [])[0].index] || {})['rango_horario'] = (responseHorario || [])[0].horario || "";
+           // ((response || [])[(responseHorario || [])[0].index] || {})['isTardanza'] = false;
 
             if (response.length - 1 == i) {
               setTimeout(() => {
