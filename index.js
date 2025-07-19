0d7f41a1abca0b5b56040c35f5103b1391025bce
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-07-19 09:29:32 -0500

    UPDATE

--- index.js
+++ index.js
@@ -1025,7 +1025,7 @@ io.on('connection', async (socket) => {
     let [arAutorizacionEmp] = await pool.query(`SELECT * FROM TB_AUTORIZAR_HR_EXTRA WHERE NRO_DOCUMENTO_EMPLEADO = '${data.nro_documento}';`);
     let [arAutorizacionResponse] = await pool.query(`SELECT * FROM TB_AROBADO_HR_EXTRA WHERE HR_EXTRA_ACOMULADO = '${data.hora_extra}' AND CODIGO_TIENDA = '${data.codigo_tienda}'  AND FECHA = '${data.fecha}' AND NRO_DOCUMENTO_EMPLEADO = '${data.nro_documento}';`);
 
-    console.log(arAutorizacionEmp[0]['NOMBRE_COMPLETO']);
+
     if (aprobado == 'rechazado') {
 
       if ((data || {}).comentario == 'No marco su salida de turno' || (data || {}).comentario == 'No marco su salida a break') {
@@ -1103,8 +1103,8 @@ io.on('connection', async (socket) => {
 
         let correo = ['itperu@metasperu.com', 'johnnygermano@metasperu.com', 'metasperurrhh@gmail.com', 'metasperurrhh2@gmail.com'];
 
-        /* emailController.sendEmail(correo, `NOTIFICACION H.EXTRA RECHAZADO POR MARCACION - ${(selectedLocal || {}).name || ''}`, bodyHTML, null, null)
-           .catch(error => res.send(error));*/
+        emailController.sendEmail(correo, `NOTIFICACION H.EXTRA RECHAZADO POR MARCACION - ${(selectedLocal || {}).name || ''}`, bodyHTML, null, null)
+          .catch(error => res.send(error));
       }
     }
 
