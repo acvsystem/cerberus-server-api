5ac9ce1d40f1a3f9035b2412721a4091b88230a3
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-08-18 12:17:44 -0500

    update

--- routes/stores.routes.js
+++ routes/stores.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 const router = Router();
-import storesController from '../controllers/csStores';
+import storesController from '../controllers/csStores.js';
 
 router.get('/all', storesController.allStores);
 
