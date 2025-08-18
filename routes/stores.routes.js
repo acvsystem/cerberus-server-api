d9acea8bbc744d414e7506046d32e46146aacfdb
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-08-18 12:07:18 -0500

    [update] - API stores/all

--- routes/stores.routes.js
+++ routes/stores.routes.js
@@ -2,7 +2,9 @@ import { Router } from 'express';
 const router = Router();
 import storesController from '../controllers/csStores.js';
 
-router.get('/all', storesController.allStores);
+router.get('/all', storesController.allStores, async (req, res) => {
+    console.log(req);
+});
 
 const storesRoutes = router;
 export default storesRoutes
