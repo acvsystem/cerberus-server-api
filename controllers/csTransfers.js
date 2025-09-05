ba08799d9c1cf02a6a152ff016b80ac2d9ef5ab5
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-09-05 11:51:55 -0500

    update

--- controllers/csTransfers.js
+++ controllers/csTransfers.js
@@ -21,7 +21,7 @@ class clsTransfers {
 
                     pool.query(`SELECT * FROM TB_DETALLE_TRASPASOS WHERE CODIGO_TRASPASO = '${code_transfers}';`).then(([requestSql]) => {
                         let indexTransfers = responseJSON.findIndex((trs) => trs.code_transfer == code_transfers);
-                        console.log(indexTransfers);
+                        console.log(code_transfers);
                         (requestSql || []).filter((detail) => {
                             (responseJSON || [])[indexTransfers]['detail'].push({
                                 barcode: detail.CODIGO_BARRA,
