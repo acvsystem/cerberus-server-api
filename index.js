1b8beab3830337dc121f4958777bbb40139ad54b
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-09-08 09:29:40 -0500

    update

--- index.js
+++ index.js
@@ -2151,7 +2151,7 @@ io.on('connection', async (socket) => {
       });
 
       let day = fechaFormateada.split('/');
-      let parseDate = `${day[0]}-${parseInt(day[1]) * 1}-${parseInt(day[2]) * 1}`;
+      let parseDate = `${dparseInt(day[0]) * 1}-${parseInt(day[1]) * 1}-${parseInt(day[2]) * 1}`;
       await pool.query(`SELECT * FROM TB_HEAD_PAPELETA WHERE ESTADO_PAPELETA != 'anulado' AND ID_PAP_TIPO_PAPELETA = 7 AND NRO_DOCUMENTO_EMPLEADO = '${(huellero || {}).nroDocumento}' AND FECHA_DESDE = '${(huellero || {}).dia}';`).then(([papeleta]) => {
         ((dataServGeneral || [])[i] || {})['papeleta'] = papeleta || [];
       });
