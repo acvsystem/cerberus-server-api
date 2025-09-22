ebe9eef15d7bdfdfb2ca24287e8124daaa1eeed9
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-09-22 10:43:31 -0500

    update

--- index.js
+++ index.js
@@ -356,6 +356,7 @@ io.on('connection', async (socket) => {
   });
 
   socket.on('report:get:fr:sales:departament:response', (response) => {
+    console.log(response);
     socket.to(`${(response || {}).socketID}`).emit('report:sales:departament:response', response);
   });
 
