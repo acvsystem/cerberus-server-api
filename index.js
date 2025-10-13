98789d068048141c58b68c68d64a2c8ebae92f96
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-10-13 09:31:23 -0500

    UPDATE

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ function onVerificarCalendario() {
 
   let day = fechaFormateada.split('/');
 
-  pool.query(`SELECT CODIGO_TIENDA FROM TB_HORARIO_PROPERTY WHERE TRIM(SUBSTRING(RANGO_DIAS,1,9)) = '${parseInt(day[0])}-${parseInt(day[1])}-${parseInt(day[2])}' GROUP BY CODIGO_TIENDA;`).then(([calendarios]) => {
+  pool.query(`SELECT CODIGO_TIENDA FROM TB_HORARIO_PROPERTY WHERE TRIM(SUBSTRING_INDEX(RANGO_DIAS, ' ', 1)) = '${parseInt(day[0])}-${parseInt(day[1])}-${parseInt(day[2])}' GROUP BY CODIGO_TIENDA;`).then(([calendarios]) => {
     let arCalendarios = ['9M'];
     (calendarios || []).filter((c) => {
       arCalendarios.push((c || {}).CODIGO_TIENDA);
