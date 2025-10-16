a9330667882b3a9bb0bb0ed56289581e7bf7ec4d
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-10-16 12:28:56 -0500

    UPDATE

--- controllers/csFacturacion.js
+++ controllers/csFacturacion.js
@@ -50,7 +50,8 @@ class clsFacturacion {
             if (!(paseDataList || []).includes(cpParse)) {
                 pool.query(`SELECT * FROM TB_DETAIL_DOCUMENT_NO_SEND WHERE NRO_DOCUMENT = '${cpParse}';`).then(([documents]) => {
                     if (!(documents || []).length) {
-                        pool.query(`INSERT INTO TB_DETAIL_DOCUMENT_NO_SEND(NRO_DOCUMENT,TYPE_DOCUMENT,DATE,EXPIRATION_DATE)VALUES('${cpParse}','${(data || {}).cmpTipo}','${(data || {}).cmpFecha}', DATE_ADD('${(data || {}).cmpFecha}', INTERVAL 3 DAY))`);
+                        pool.query(`INSERT INTO TB_DETAIL_DOCUMENT_NO_SEND(NRO_DOCUMENT,TYPE_DOCUMENT,DATE,EXPIRATION_DATE,STATUS,OWNER)
+                            VALUES('${cpParse}','${(data || {}).cmpTipo}','${(data || {}).cmpFecha}', DATE_ADD('${(data || {}).cmpFecha}', INTERVAL 3 DAY),'PENDING','FRONT RETAIL')`);
                     }
                 });
 
