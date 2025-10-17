f398ba8ad37813829b5a5dd43994995f6f34c0c3
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-10-17 15:12:52 -0500

    update

--- routes/security.routes.js
+++ routes/security.routes.js
@@ -133,7 +133,7 @@ router.post('/in/document/pending', async (req, res) => {
 
 router.post('/del/document/pending', async (req, res) => {
     let documents = (req || {}).body || [];
-    console.log(documents);
+    console.log("DELETE",documents);
     (documents || []).filter((doc) => {
         pool.query(`DELETE FROM TB_DETAIL_DOCUMENT_NO_SEND WHERE NRO_DOCUMENT = '${doc.nro_document}';`)
     });
