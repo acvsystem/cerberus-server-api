f403717d939281668a1d5a21075d3d4d004abd11
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-10-18 12:08:13 -0500

    UPDATE

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ function vrfDocumentPending() {
       }
 
       if (documents.length - 1 == i) {
-        if (!(data || {})['EXPIRATION_DATE']) {
+        if (!(data || {})['ISNOTIFICATED']) {
           if ((documentPending || []).length) {
             let bodyHTML = `<table style="width:100%;border-spacing:0">
                 <tbody>
