52cca98de5278febdd9dfaf22c71a855e9ee1bb5
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-10-18 12:14:43 -0500

    update

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ function vrfDocumentPending() {
       }
 
       if (documents.length - 1 == i) {
-        if (!(data || {})['ISNOTIFICATED']) {
+        if ((data || {})['ISNOTIFICATED'] == 0) {
           if ((documentPending || []).length) {
             let bodyHTML = `<table style="width:100%;border-spacing:0">
                 <tbody>
