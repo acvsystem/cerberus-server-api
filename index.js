7ebf118e85ef2eb2b737d4d8879374fb9373d998
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-11-10 15:49:10 -0500

    update

--- index.js
+++ index.js
@@ -635,7 +635,7 @@ io.on('connection', async (socket) => {
         (tienda || []).filter(async (td, i) => {
           tiendasList.push({ code: (td || {}).SERIE_TIENDA, name: (td || {}).DESCRIPCION });
 
-          if (tienda.length == tiendasList.length) {
+          if (tiendasList.length -1 == i) {
             let listSessionConnect = await facturacionController.verificacionDocumentos({ serverData: resData['serverData'], frontData: resData['frontData']['data'], codigoFront: resData['codigoFront'] }, tiendasList);
 
             socket.to(`${socketID}`).emit("comprobantes:get:response", listSessionConnect); // SE ENVIA A FRONTEND
