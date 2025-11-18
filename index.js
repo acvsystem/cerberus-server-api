d206365001637ad40aa094cf8f8da7ebd1515086
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-11-18 11:34:25 -0500

    update

--- index.js
+++ index.js
@@ -609,7 +609,7 @@ io.on('connection', async (socket) => {
 
   socket.on('emitCleanClient', (data) => {
     let socketID = (socket || {}).id;
-    socket.broadcast.emit("limpiarCliente", { clientes: listCliente }, socketID, true);
+    socket.broadcast.emit("limpiarCliente", { clientes: listCliente, isCallGoblar: true }, socketID);
   });
 
   socket.on('cleanColaFront', (data) => {
