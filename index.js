b9f071a1945432cdfea101ab1bd916d4dc280d7e
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-11-24 15:13:39 -0500

    update

--- index.js
+++ index.js
@@ -135,18 +135,17 @@ const task_11 = cron.schedule('2 7 * * *', () => {
   onEmitJobMallAventura(3);
 });
 
-const task_12 = cron.schedule('0 15 * * *', () => {
+const task_12 = cron.schedule('14 15 * * *', () => {
   console.log('0 15 * * *','onEmitJobMallAventura(1)');
-  console.log('0 15 * * *')
   onEmitJobMallAventura(1);
 });
 
-const task_13 = cron.schedule('1 15 * * *', () => {
+const task_13 = cron.schedule('15 15 * * *', () => {
   console.log('1 15 * * *','onEmitJobMallAventura(2)');
   onEmitJobMallAventura(2);
 });
 
-const task_14 = cron.schedule('2 15 * * *', () => {
+const task_14 = cron.schedule('16 15 * * *', () => {
   console.log('2 15 * * *','onEmitJobMallAventura(3)');
   onEmitJobMallAventura(3);
 });
