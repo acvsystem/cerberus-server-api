c592cb821c979496ded169211c472ac4be222c43
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-12-10 09:28:17 -0500

    update

--- index.js
+++ index.js
@@ -442,7 +442,7 @@ io.on('connection', async (socket) => {
   if (codeTerminal != "SRVFACT" && isIcg != 'true') {
     let listSessionConnect = await sessionSocket.connect(codeTerminal);
 
-    emitClearClient(codeTerminal);
+    //emitClearClient(codeTerminal);
 
     socket.broadcast.emit("comprobantes:get:response", listSessionConnect);
   } else {
