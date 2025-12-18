96ae63c9a1d11b1f374f0e8ccaf8f5bf12a79b7d
acvsystem <36681777+acvsystem@users.noreply.github.com>
2025-12-18 15:01:44 -0500

    update

--- index.js
+++ index.js
@@ -173,7 +173,7 @@ const task_1 = cron.schedule('00 10 * * *', () => {
 
   pool.query(`SELECT * FROM TB_CLIENTES_CLEAR_FORNT;`).then(([data]) => {
     let listCliente = ((data || [])[0]['LIST_CLIENTE']).split(',');
-    io.emit("limpiarCliente", { clientes: listCliente, codeStore: '1', isCallGoblar: true }, 'backend');
+    io.emit("SrvlimpiarCliente", { clientes: listCliente, codeStore: '1', isCallGoblar: true }, 'backend');
   });
 });
 
@@ -216,7 +216,7 @@ const task_9 = cron.schedule('00 22 * * 0', () => {
   console.log('00 22 * * 0')
   pool.query(`SELECT * FROM TB_CLIENTES_CLEAR_FORNT;`).then(([data]) => {
     let listCliente = ((data || [])[0]['LIST_CLIENTE']).split(',');
-    io.emit("limpiarCliente", { clientes: listCliente, codeStore: '2', isCallGoblar: true }, 'backend');
+    io.emit("SrvlimpiarCliente", { clientes: listCliente, codeStore: '2', isCallGoblar: true }, 'backend');
   });
 });
 
@@ -293,7 +293,7 @@ function emitClearClient(code) {
   console.log("emitClearClient****************************************************");
   pool.query(`SELECT * FROM TB_CLIENTES_CLEAR_FORNT;`).then(([data]) => {
     let listCliente = ((data || [])[0]['LIST_CLIENTE']).split(',');
-    io.emit("limpiarCliente", { clientes: listCliente, codeStore: "3", isCallGoblar: false }, "backend");
+    io.emit("limpiarCliente", { clientes: listCliente, codeStore: code || "backend", isCallGoblar: false }, "backend");
   });
 }
 
@@ -531,7 +531,7 @@ io.on('connection', async (socket) => {
   if (codeTerminal != "SRVFACT" && isIcg != 'true') {
     let listSessionConnect = await sessionSocket.connect(codeTerminal);
 
-    //emitClearClient(codeTerminal);
+    emitClearClient(codeTerminal);
 
     socket.broadcast.emit("comprobantes:get:response", listSessionConnect);
   } else {
@@ -703,7 +703,7 @@ io.on('connection', async (socket) => {
     let socketID = (socket || {}).id;
     pool.query(`SELECT * FROM TB_CLIENTES_CLEAR_FORNT;`).then(([data]) => {
       let listCliente = ((data || [])[0]['LIST_CLIENTE']).split(',');
-      socket.broadcast.emit("limpiarCliente", { clientes: listCliente, codeStore: '4', isCallGoblar: true }, socketID);
+      socket.broadcast.emit("SrvlimpiarCliente", { clientes: listCliente, codeStore: '4', isCallGoblar: true }, socketID);
     });
   });
 
