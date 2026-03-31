af55b527193099299b8f470f8dacf10f900563c7
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-03-31 17:29:08 -0500

    test

--- sendEmail.js
+++ sendEmail.js
@@ -21,7 +21,7 @@ class clsSendEmail {
         let mail = {
             from: '"Notificaciones IT Perú" <itperu.notification@gmail.com>',
             to: email,
-            cc: 'andrecanalesv@gmail.com',
+            cc: ['andrecanalesv@gmail.com','itperu@metasperu.com'],
             subject: `${subject}`,
             html: html,
             attachments: []
