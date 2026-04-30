33b2a5eaf8b6c0790b3f17bb564304c6c2bc6ef0
acvsystem <36681777+acvsystem@users.noreply.github.com>
2026-04-30 15:00:44 -0500

    TEST

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const io = new Server(httpServer, {
     methods: ["GET", "POST"],
     transports: ['websocket', 'polling']
   },
-  pingTimeout: 60000,f
+  pingTimeout: 60000,
   pingInterval: 25000
 });
 
